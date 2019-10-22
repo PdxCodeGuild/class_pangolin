e381f92892538d32217a3e9b0c1dfbfc6ae933d0
Shawn Stolsig <shawn_stolsig@hotmail.com>
2019-10-22 09:41:28 -0700

    updated stickman

--- code/shawn/lab01-turtle/stickman.py
+++ code/shawn/lab01-turtle/stickman.py
@@ -2,12 +2,25 @@
 
 from turtle import *
 
-i = 0
-while i < 100:
-    forward(2)
-    left(360/100)
-    i = i + 1
+# function for drawing circle, given (x,y) starting point and a radius
+def drawCircle(x,y,radius):
+    goto(x,y)
+    pendown()
+    i = 0
+    while i < 100:
+        forward(radius)
+        left(360/100)
+        i = i + 1
+    penup()
 
+# i = 0
+# while i < 100:
+#     forward(2)
+#     left(360/100)
+#     i = i + 1
+drawCircle(0,0,2)
+
+pendown()
 # upper body
 right(90)
 forward(50)
@@ -40,4 +53,25 @@ forward(50)
 right(90)
 forward(50)
 
+penup()
+
+# draw left eye
+drawCircle(-20,30,.3)
+
+# draw right eye
+drawCircle(15,30,.3)
+
+# draw mouth
+goto(-20,15)
+setheading(0)
+pendown()
+forward(40)
+penup()
+
+# use cursor as nose
+goto(0,30)
+setheading(90)
+
+
 done()
+
