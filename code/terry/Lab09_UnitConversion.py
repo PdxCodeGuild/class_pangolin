2a1840d7b5bccfe96338e48c86e9fb3fb32836d4
terry <tlfuller@rocketmail.com>
2019-10-22 15:50:01 -0700

    Checks to see if the user entered anything or not

--- code/terry/Lab09_UnitConversion.py
+++ code/terry/Lab09_UnitConversion.py
@@ -2,10 +2,11 @@
 # Other versions of this program will expand off this one.
 aMeter = 0.3048
 
-numOfFeet = int(input("What is the distance in feet? "))
-# if not str(numOfFeet):
-#     print("You didn't enter a number.")
-# print(numOfFeet)
-x = numOfFeet * aMeter
-x = str(round(x, 3))
-print(f"{numOfFeet}ft is {x}m")
+numOfFeet = input("What is the distance in feet? ")
+if numOfFeet:
+    numOfFeet = int(numOfFeet)
+    x = numOfFeet * aMeter
+    x = str(round(x, 3))
+    print(f"{numOfFeet}ft is {x}m")
+else:
+    print("You did not enter a value.")
