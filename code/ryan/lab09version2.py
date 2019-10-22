7ab7106622d9f8b59b4aa96ae9d7bba17f4cc703
Ryan Collins <ryan.k.collins222@gmail.com>
2019-10-22 15:55:54 -0700

    version 1

---
+++ code/ryan/lab09version2.py
@@ -0,0 +1,11 @@
+unit = input("What unit of measurement would you like to convert to meters?  Feet, miles, or kilometers?:  ").lower()
+num = int(input(f"How many {unit} would you like to convert?  Please give a whole number:  "))
+
+if unit == "feet":
+    result = num * .3048
+elif unit == "miles":
+    result = num * 1609.34
+else:
+    result = num * 1000
+
+print(f"You entered {num} {unit}.  This equals {result} meters.")
