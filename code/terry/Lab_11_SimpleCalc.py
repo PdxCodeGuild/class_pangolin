aa57415f794222d7d26dcfbc5235e2666e75fc88
terry <tlfuller@rocketmail.com>
2019-10-23 14:16:27 -0700

    Lab 11 - working version 1 and 2

--- code/terry/Lab_11_SimpleCalc.py
+++ code/terry/Lab_11_SimpleCalc.py
@@ -3,10 +3,28 @@
 num1 = 0
 num2 = 0
 operator = ""
+outPut = 0.0
 finish = True
 
 while finish:
     operator = input("What operation do you want to perform? ")
-    num1 = input("What is the first number? ")
-    num2 = input("What is the second number? ")
 
+    if operator.lower() == 'done':
+        finish = False
+        break
+    else:
+        num1 = input("What is the first number? ")
+        num2 = input("What is the second number? ")
+        num1 = float(num1)
+        num2 = float(num2)
+        if operator == '+':
+            outPut = num1 + num2
+        elif operator == '-':
+            outPut = num1 - num2
+        elif operator == '*':
+            outPut = num1 * num2
+        elif operator == '/':
+            outPut = num1 / num2
+        print(outPut)
+#finish = False
+#break
