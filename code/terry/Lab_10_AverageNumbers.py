2e24b673cf90c03233820801e43f1fa0b819b6a6
terry <tlfuller@rocketmail.com>
2019-10-23 12:57:49 -0700

    Version 2 - Final

--- code/terry/Lab_10_AverageNumbers.py
+++ code/terry/Lab_10_AverageNumbers.py
@@ -7,20 +7,27 @@ for num in numbers:
 
 outPut = outPut / len(numbers)
 outPut = round(outPut, 2)
+print("Version 1")
 print(outPut)
 
 # Average a list of numbers entered by the user.
 
 numbers2 = []
 userInput = ""
+outPut2 = 0
 finish = True
 
+print("\nVersion 2")
 while finish:
+
     userInput = input("Please enter a number or 'Done' to finish. ")
-    if userInput == 'Done':
+    if userInput.lower() == 'done':
         finish = False
     else:
         numbers2.append(userInput)
+        outPut2 += int(userInput)
 
-print(userInput)
-print(numbers2)
+outPut2 = outPut2 / len(numbers2)
+outPut2 = round(outPut2, 2)
+
+print(f"The average of the numbers is: {outPut2}")
