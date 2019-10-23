2df5e0cf78b53bb75d3b3637e074e76ec27e39c3
Michael R.T <cloudleviathan@outlook.com>
2019-10-22 20:07:40 -0700

    Reduced code

--- code/taylor/unit_converter_v4.py
+++ code/taylor/unit_converter_v4.py
@@ -2,7 +2,7 @@
 # PDX Code Guild
 # Lab 09 Complete
 
-# Conversions to meters formula
+# Variables
 ft = .3048
 mi = 1609.34
 km = 1000
@@ -10,39 +10,37 @@ m = 1
 yd = .9144
 nchz = .0254 
 
-message = "Enter the length / distance as a number: > "
+message1 = "Enter the length / distance as a number: > "
+message2 = "Enter a starting uint as (ft, mi, km, m, yd, nchz): > "
+message3 = "Enter a conversion uint as (ft, mi, km, m, yd, nchz): > "
 
-# Input function from 101computing.net 
+# Numeric input validation function from 101computing.net 
 def inputNumber(message):
      while True:
           try:
                user_number_input = int(input(message)) 
           except ValueError:
-               print("Please follow the instructions")
+               print("\nPlease follow the instructions")
                continue
           else:
                return user_number_input
-              
+# Unit input validation function
+def inputUnits(message):
+     while True:
+          user_unit = input(message)
+          if user_unit.lower() not in ("ft", "mi", "km","m", "yd", "nchz"):
+               print("\nPlease follow the instructions")
+          else:
+               return user_unit
+                        
 # Greets the user
 print("\nWelcome, this tool is a conversion calculator for (ft, mi, km, m, yd, nchz)\n")
 
-# User input with validation
-while True:
-     user_starting_unit = input("Enter a starting uint as (ft, mi, km, m, yd, nchz): > ")
-     if user_starting_unit.lower() not in ("ft", "mi", "km","m", "yd", "nchz"):
-          print("Please follow the instructions")
-     else:
-          break
+# Function call inputUnits
+user_starting_unit = inputUnits(message2)
 
 # Calls the input function for the numerical length / distance     
-user_number_input = inputNumber(message)
-
-while True:
-     user_unit_conversion_input = input("Enter an ending unit as (ft, mi, km, m, yd, nchz): > ")
-     if user_unit_conversion_input.lower() not in ("ft", "mi", "km","m", "yd", "nchz"):
-          print("Please follow the instructions")
-     else:
-          break
+user_number_input = inputNumber(message1)
 
 # Converts the starting unit to meters
 if user_starting_unit == "ft":
@@ -57,7 +55,10 @@ elif user_starting_unit == "nchz":
      to_meters = nchz * user_number_input
 else:
      to_meters = m * user_number_input
-     
+
+# Function call inputUnits
+user_unit_conversion_input =inputUnits(message3)
+
 # Final conversion from meters to desired unit
 if user_unit_conversion_input == "ft":
      print(to_meters / ft)
