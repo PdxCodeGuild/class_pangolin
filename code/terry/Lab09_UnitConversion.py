18f4cba76b2d40ce797b151697191f513fe819d0
terry <tlfuller@rocketmail.com>
2019-10-22 22:34:03 -0700

    Version 4 - Final version

--- code/terry/Lab09_UnitConversion.py
+++ code/terry/Lab09_UnitConversion.py
@@ -24,37 +24,39 @@ userDistance = 0
 userUnit = input("What is the unit input? From: ").lower()
 userOutput = input("What is the unit output? To: ").lower()
 userDistance = input("What is the distance to be converted? ")
-# print(userUnit)
-# print(userDistance)
-if userUnit == "mi":
-    x = aMile * float(userDistance)
-elif userUnit == "ft":
-    x = 0.3048 * float(userDistance)
-elif userUnit == "m":
-    x = float(userDistance)
-elif userUnit == "km":
-    x = 1000 * float(userDistance)
-elif userUnit == "yd":
-    x = aYard * float(userDistance)
-elif userUnit == "in":
-    x = 0.0254 * float(userDistance)
 
-print(x)
-print(userOutput)
+if userUnit == userOutput:
+    print("These units are the same.")
+elif int(userDistance) <= 0:
+    print("Enter a number above zero.")
+else:
+    # Start of the Input.
+    if userUnit == "mi":
+        x = 1609.34 * float(userDistance)
+    elif userUnit == "ft":
+        x = 0.3048 * float(userDistance)
+    elif userUnit == "m":
+        x = float(userDistance)
+    elif userUnit == "km":
+        x = 1000 * float(userDistance)
+    elif userUnit == "yd":
+        x = 0.9144 * float(userDistance)
+    elif userUnit == "in":
+        x = 0.0254 * float(userDistance)
 
-if userOutput == "mi":
-    y = 0.000621
-elif userOutput == "ft":
-    y = 3.281
-elif userOutput == "m":
-    y = 1
-elif userOutput == "km":
-    y = 0.001
-elif userOutput == "yd":
-    y = 1.094
-elif userOutput == "in":
-    y = 39.370
+    # Start of the Output
+    if userOutput == "mi":
+        y = 0.000621
+    elif userOutput == "ft":
+        y = 3.281
+    elif userOutput == "m":
+        y = 1
+    elif userOutput == "km":
+        y = 0.001
+    elif userOutput == "yd":
+        y = 1.094
+    elif userOutput == "in":
+        y = 39.370
 
-print(y)
-k = x * y
-print(f"{userDistance} {userUnit} to {userOutput} is: {k}")
+    k = x * y
+    print(f"{userDistance} {userUnit} to {userOutput} is approx: {k}")
