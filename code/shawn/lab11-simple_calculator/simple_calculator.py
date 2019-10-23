c251f4c67971bfe056c92412d72d956d53e812de
Shawn Stolsig <shawn_stolsig@hotmail.com>
2019-10-23 14:00:57 -0700

    version 2 done

--- code/shawn/lab11-simple_calculator/simple_calculator.py
+++ code/shawn/lab11-simple_calculator/simple_calculator.py
@@ -5,3 +5,34 @@
 
 # ( version 3: might not be possible since they've limited literal_eval
 # to not execute expressions....safe version of eval called ast.literal_eval  must import ast  )
+
+# main loop
+while True:
+    # get operator input
+    op = input("What is the operation you'd like to perform? ")
+
+    # create loop exit
+    if op == "done":
+        break
+
+    # get number input
+    num1 = float(input("What is the first number? "))
+    num2 = float(input("What is the second number? "))
+
+    # otherwise, print operation
+    if op == '+':
+        print(f"{num1} {op} {num2} = {num1 + num2}")
+    elif op == '-':
+        print(f"{num1} {op} {num2} = {num1 - num2}")
+    elif op == '/':
+        print(f"{num1} {op} {num2} = {num1 / num2}")
+    elif op == '*':
+        print(f"{num1} {op} {num2} = {num1 * num2}")
+    elif op == '%':
+        print(f"{num1} {op} {num2} = {num1 % num2}")
+    elif op == '**':
+        print(f"{num1} {op} {num2} = {num1 ** num2}")
+    else:
+        print("unknown operator, restarting")
+
+print('goodbye!')
