f4852f2ce51a97e5e661b7620251542e2044667f
terry <tlfuller@rocketmail.com>
2019-10-23 13:16:40 -0700

    Lab 11 - basic variables set up.

---
+++ code/terry/Lab_11_SimpleCalc.py
@@ -0,0 +1,12 @@
+# Design a simple calculator.  Allows the user to define an operator and numbers to add.
+
+num1 = 0
+num2 = 0
+operator = ""
+finish = True
+
+while finish:
+    operator = input("What operation do you want to perform? ")
+    num1 = input("What is the first number? ")
+    num2 = input("What is the second number? ")
+
