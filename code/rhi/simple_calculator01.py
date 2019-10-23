506f1d9efcfb7fb35d8f903610b19cf8df79c7f8
unknown <rhiraed@gmail.com>
2019-10-23 16:28:45 -0700

    simple_calculator01

--- code/rhi/simple_calculator01.py
+++ code/rhi/simple_calculator01.py
@@ -6,23 +6,35 @@ last updated oct 23 2019
 op = input('What mathmatical operation would you like to perform?: ')
 num1 = int(input('What is the first number you would like to enter?: '))
 num2 = int(input('What is the second number you would like to enter?: '))
-#num1 = [int(i) for i in num1]
-#num2 = [int(i) for i in num2]
+
 if op == '+':
     result = (num1 + num2)
+    result = round(result, 3)
+    print(f'{num1} {op} {num2} = {result}')
 elif op == '-':
     result = (num1 - num2)
+    result = round(result, 3)
+    print(f'{num1} {op} {num2} = {result}')
 elif op == '*':
     result = (num1 * num2)
+    result = round(result, 3)
+    print(f'{num1} {op} {num2} = {result}')
 elif op == '/':
     result = (num1 / num2)
+    result = round(result, 3)
+    print(f'{num1} {op} {num2} = {result}')
 elif op == '//':
     result = (num1 // num2)
+    result = round(result, 3)
+    print(f'{num1} {op} {num2} = {result}')
 elif op == '%':
     result = (num1 % num2)
+    result = round(result, 3)
+    print(f'{num1} {op} {num2} = {result}')
 elif op == '**':
     result = (num1 ** num2)
+    result = round(result, 3)
+    print(f'{num1} {op} {num2} = {result}')
 else:
     print('not a valid operation!')
-result = round(result, 3)
-print(f'{num1} {op} {num2} = {result}')
+print('Thank you and we hope to see you again!')
