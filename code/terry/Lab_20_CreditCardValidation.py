bf9f6be7a577cf930e17c4aaf73ea6874b6620ff
terry <tlfuller@rocketmail.com>
2019-10-23 23:13:53 -0700

    Lab 20 - Most of it working.  Need to update the 'double'.  Also check for user input issues.

--- code/terry/Lab_20_CreditCardValidation.py
+++ code/terry/Lab_20_CreditCardValidation.py
@@ -17,22 +17,33 @@ no alpha needed in this lab
 """
 check_digit = 0
 cc_input = 0
+card_valid = False
 cc_input = input("Please input the CC number: ")
 cc_input = [int(i) for i in cc_input]
-print(f"The original list {cc_input}")
+print(f"The original list is: {cc_input}")
 
 check_digit = cc_input.pop()
-print(f"The check digit is {check_digit}")
+print(f"The check digit is: {check_digit}")
 
 cc_input = list(reversed(cc_input))
-print(f"The reversed list {cc_input}")
+print(f"The reversed list is: {cc_input}")
 
 cc_input = [d * 2 for d in cc_input]
-print(f"The doubled list is {cc_input}")
+print(f"The doubled list is: {cc_input}")
 
-nine = [n - 9 if n > 9 else n for n in cc_input]
-print(f"The 'nine' list is {nine}")
+cc_input = [n - 9 if n > 9 else n for n in cc_input]
+print(f"The 'nine' list is: {cc_input}")
 
-# for i in range(len(card_number_list)):
-# 		if card_number_list[i] > 9:
-# 			card_number_list[i] -= 9
+inputSum = sum(cc_input)
+print(f"The sum of the list is: {inputSum}")
+inputSum = str(inputSum)
+
+inputSum = inputSum[1]
+
+if inputSum == check_digit:
+    card_valid = True
+else:
+    card_valid = False
+print(f"The check digit is: {check_digit}")
+print(f"The second digit of the sum is: {inputSum}")
+print(f"Is the card valid? {card_valid}")
