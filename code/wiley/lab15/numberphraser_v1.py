f2eb0051a6f9873d432a60c387d56ba0c111b2ef
WileyRummel <wiley.rummel@gmail.com>
2019-10-25 15:31:01 -0700

    lab15 both versions. Good enough!

--- code/wiley/lab15/numberphraser_v1.py
+++ code/wiley/lab15/numberphraser_v1.py
@@ -6,19 +6,21 @@
 
 #make dictionary for ten digits
 #Still need to figure out how to deal with teens.  Hard code if begins with 1_?
-tens_digit_dict = {2:"Twenty", 3:"Thirty",4:"Forty",5:"Fifty",6:"Sixty",7:"Seventy",8:"Eighty",9:"Ninety"}
+tens_digit_dict = {0: "",2:"Twenty", 3:"Thirty",4:"Forty",5:"Fifty",6:"Sixty",7:"Seventy",8:"Eighty",9:"Ninety"}
 teens_digit_dict = {10:"Ten", 11: "Eleven", 12:"Twelve",13:"Thirteen",14:"Fourteen",15:"Fifteen",16:"Sixteen",17:"Seventeen",18:"Eighteen",19:"Nineteen"}
 #make dictionary for ones digits
-ones_digit_dict = {1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine"}
+ones_digit_dict = {0: "", 1:"one",2:"two",3:"three",4:"four",5:"five",6:"six",7:"seven",8:"eight",9:"nine"}
 
 #make function to take in number inputs
 
 def phraser ():
-    a = int(input("What is the number?"))
-    if a in teens_digit_dict.keys():
-        return print(teens_digit_dict.get(a))
+    a = int(input("What is the number? \n"))
     tens_digit = a//10
     ones_digit = a%10
+    if a ==0:
+        return print("zero")
+    if a in teens_digit_dict.keys():
+        return print(teens_digit_dict.get(a))
     if tens_digit in tens_digit_dict.keys():
         #print(tens_digit_dict.get(tens_digit))
         first = tens_digit_dict.get(tens_digit)
