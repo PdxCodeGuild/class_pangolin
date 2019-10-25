11168cf426f14ec2b6a479242d43e2a8ac705377
Shawn Stolsig <shawn_stolsig@hotmail.com>
2019-10-25 11:51:30 -0700

    adding intro lab 10 advanced ver 2

--- code/shawn/lab19-blackjack_advice/blackjack_advice.py
+++ code/shawn/lab19-blackjack_advice/blackjack_advice.py
@@ -1,7 +1,7 @@
 # Shawn Stolsig
 # PDX Code Guild 
 # Assignment: Lab 19 - Blackjack Advice
-# Date: 10/24/2019
+# Date: 10/25/2019
 
 # you could make a while loop instead of only requiring three cards
 # make it a whole blackjack game by making the computer the house
