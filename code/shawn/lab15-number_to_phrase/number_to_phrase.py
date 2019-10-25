ef258c0d4986e6c714b3dc9aae862603c3aa4bbc
Shawn Stolsig <shawn_stolsig@hotmail.com>
2019-10-24 17:45:04 -0700

    intro lab 9, added ability to create custom currency

---
+++ code/shawn/lab15-number_to_phrase/number_to_phrase.py
@@ -0,0 +1,9 @@
+# Shawn Stolsig
+# PDX Code Guild 
+# Assignment: Lab 15 - Number to Phrase
+# Date: 10/24/2019
+
+# use modulus or string operations to break things up to digits
+# use the digit as an index for a list of strings or as a key for a dict of digit:phrase pairs
+# ver 3 and 4 are optional
+# make function to do this, then call the function
