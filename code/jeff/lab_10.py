3a15eb2ded042f5d721c69763e3a19cf9bfe7130
Jeff <jsportland@gmail.com>
2019-10-25 15:42:57 -0700

    added 2 labs

--- code/jeff/lab_10.py
+++ code/jeff/lab_10.py
@@ -2,21 +2,12 @@
 # Jeff Smith
 # 10/23/19
 # Average numbers
+
 import math
+
 nums = [5, 0, 8, 3, 4, 1, 6]
 
-#for [i] in nums:
-#    print(num)
-
-#for i in range(len(nums)):
-#    print(nums[i])
-
-#i = 0
 for i in range(len(nums)):
-    print(sum(nums)/len(nums))
-
-squares = [i**2 for i in range(1,11)]
-print(squares)
-print(list(range(1,11)))
-
-distances_km = [round(distance * 1.6, 1) for distance in distances]
+    avg = (sum(nums)/len(nums))
+avg = round(avg, 1)
+print(avg)
