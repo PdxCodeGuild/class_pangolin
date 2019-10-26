b421011866d80288cc08f2e2ab99769760e3151d
terry <tlfuller@rocketmail.com>
2019-10-25 21:57:00 -0700

    Lab 15 - Version 2 working version, need to create functions

--- code/terry/Lab_15_NumToPhrase_3.py
+++ code/terry/Lab_15_NumToPhrase_3.py
@@ -1,4 +1,4 @@
-num_dict = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine",
+num_dict = {0: "Zero", 1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "Seven", 8: "Eight", 9: "Nine",
             10: "Ten", 11: "Eleven", 12: "Twelve", 13: "Thirteen", 14: "Fourteen", 15: "Fifteen", 16: "Sixteen",
             17: "Seventeen", 18: "Eighteen", 19: "Nineteen", 20: "Twenty", 30: "Thirty", 40: "Forty", 50: "Fifty",
             60: "Sixty", 70: "Seventy", 80: "Eighty", 90: "Ninety", 100: "Hundred"}
@@ -8,6 +8,7 @@ userInputInt = int(userInput)
 # outPut = []
 ifTen = False
 if100 = False
+ifOnes = False
 
 userInputStr = list(userInput)
 print(userInputStr)
@@ -16,16 +17,10 @@ print(userInputLst)
 
 if userInputInt >= 100:
     if100 = True
-    num1 = int(userInputLst[0])
-    num2 = int(userInputLst[1])
-    num3 = int(userInputLst[2])
-
 elif userInputInt <= 99 and userInputInt > 9:
     ifTen = True
-    num1 = int(userInputLst[0])
-    num2 = int(userInputLst[1])
 else:
-    num1 = int(userInputLst[0])
+    ifOnes = True
 
 i = 0
 testWord2 = []
@@ -42,5 +37,21 @@ if if100:
         testWord2.append(num_dict[num3])
         i += 1
     testWord2.insert(1, "Hundred")
+
+if ifTen:
+    num1 = int(userInputStr[0])
+    num1 *= 10
+    if num1 in num_dict:
+        testWord2.append(num_dict[num1])
+    num2 = int(userInputStr[1])
+    if num2 in num_dict:
+        testWord2.append(num_dict[num2])
+        i += 1
+
+if ifOnes:
+    num1 = int(userInputStr[0])
+    if num1 in num_dict:
+        testWord2.append(num_dict[num1])
+        i += 1
 # testWord = [[i] for i in outPut]
 print(testWord2)
