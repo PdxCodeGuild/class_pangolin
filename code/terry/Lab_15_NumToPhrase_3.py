cdf61afdfb93739ba21531fb0300e9239109ffce
terry <tlfuller@rocketmail.com>
2019-10-25 21:42:30 -0700

    Lab 15 - Version 2, Got "words" working.  Updated 100's

--- code/terry/Lab_15_NumToPhrase_3.py
+++ code/terry/Lab_15_NumToPhrase_3.py
@@ -5,7 +5,7 @@ num_dict = {1: "One", 2: "Two", 3: "Three", 4: "Four", 5: "Five", 6: "Six", 7: "
 
 userInput = input("Enter the number to be converted to English. ")
 userInputInt = int(userInput)
-outPut = []
+# outPut = []
 ifTen = False
 if100 = False
 
@@ -16,13 +16,31 @@ print(userInputLst)
 
 if userInputInt >= 100:
     if100 = True
-
-for userInputInt in userInputLst:
     num1 = int(userInputLst[0])
-    if userInputInt in num_dict:
-        outPut.append(num_dict[userInputInt])
+    num2 = int(userInputLst[1])
+    num3 = int(userInputLst[2])
 
+elif userInputInt <= 99 and userInputInt > 9:
+    ifTen = True
+    num1 = int(userInputLst[0])
+    num2 = int(userInputLst[1])
+else:
+    num1 = int(userInputLst[0])
+
+i = 0
+testWord2 = []
 if if100:
-    outPut.insert(1, "Hundred")
-
-print(outPut)
+    num1 = int(userInputStr[0])
+    if num1 in num_dict:
+        testWord2.append(num_dict[num1])
+    num2 = int(userInputStr[1])
+    num2 *= 10
+    if num2 in num_dict:
+        testWord2.append(num_dict[num2])
+    num3 = int(userInputStr[2])
+    if num3 in num_dict:
+        testWord2.append(num_dict[num3])
+        i += 1
+    testWord2.insert(1, "Hundred")
+# testWord = [[i] for i in outPut]
+print(testWord2)
