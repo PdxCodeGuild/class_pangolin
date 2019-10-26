cedeaba0bd3cfba2967818885636de93cb8487c9
Michael R.T <cloudleviathan@outlook.com>
2019-10-25 19:33:42 -0700

    Cleaned up and added a main loop to Lab_15

--- code/taylor/Lab_15_19/number_to_phrase.py
+++ code/taylor/Lab_15_19/number_to_phrase.py
@@ -2,50 +2,15 @@
 # PDX Coed Guild
 # Lab_10v2
 # 10/25/2019
+# Wanted to try it without the % and //... Things got out of hand O_o
 
 # Variables
 message1 = "\nEnter a number (0 - 999): > "
 
-# User input
-usr_inp1 = input(message1)
-
-num = usr_inp1
-
 # Number to English dictionaries
-num_sgl = {
-     "0": "Zero",
-     "1": "One",
-     "2": "Two",
-     "3": "Three",
-     "4": "Four",
-     "5": "Five",
-     "6": "Six",
-     "7": "Seven",
-     "8": "Eight",
-     "9": "Nine"
-}
-num_dbl = {
-     "0": "Ten",
-     "1": "Eleven",
-     "2": "Twelve",
-     "3": "Thirteen",
-     "4": "Fourteen",
-     "5": "Fifteen",
-     "6": "Sixteen",
-     "7": "Seventeen",
-     "8": "Eighteen",
-     "9": "Nineteen"
-}
-num_tns = {
-     "2": "Twenty",
-     "3": "Thirty",
-     "4": "Fourty",
-     "5": "Fifty",
-     "6": "Sixty",
-     "7": "Seventy",
-     "8": "Eighty",
-     "9": "Ninety",
-}
+num_sgl = {"0": "Zero","1": "One","2": "Two","3": "Three","4": "Four","5": "Five","6": "Six","7": "Seven","8": "Eight","9": "Nine"}
+num_dbl = {"0": "Ten","1": "Eleven","2": "Twelve","3": "Thirteen","4": "Fourteen","5": "Fifteen","6": "Sixteen","7": "Seventeen","8": "Eighteen","9": "Nineteen"}
+num_tns = {"2": "Twenty","3": "Thirty","4": "Fourty","5": "Fifty","6": "Sixty","7": "Seventy","8":"Eighty","9": "Ninety",}
 
 # Get lenghth of the input string
 def getInpLen(num_str):
@@ -74,7 +39,7 @@ def getTensValue(usr_inp1):
         pos1 = getNumDict(num_tns, numToList(usr_inp1)[0])
         print(pos1)
 
-# Gets the value for 3 digits
+# Gets the value for 3 digits > Below is where I started to have regrets
 def getHunValue(usr_inp1):
     if numToList(usr_inp1)[1] == "0":
         if numToList(usr_inp1)[2] == "0":
@@ -99,13 +64,20 @@ def getHunValue(usr_inp1):
                 pos2 = getNumDict(num_tns, numToList(usr_inp1)[1])
                 print(f"{pos1}-Hundred-{pos2}")
 
-# Main program
-if getInpLen(usr_inp1) == 1:
-    print(getNumDict(num_sgl, usr_inp1))
-
-if getInpLen(usr_inp1) == 2:
-    getTensValue(usr_inp1)
-
-if getInpLen(usr_inp1) == 3:
-    getHunValue(usr_inp1)
-   
+# Main loop
+while True:
+ 
+    try:
+        usr_inp = int(input(message1))
+        usr_inp1 = str(usr_inp)
+        print("Ctrl + C to exit\n")
+        if usr_inp > 999:
+            print("I can't count that high :(")
+        elif getInpLen(usr_inp1) == 1:
+            print(getNumDict(num_sgl, usr_inp1))
+        elif getInpLen(usr_inp1) == 2:
+            getTensValue(usr_inp1)
+        elif getInpLen(usr_inp1) == 3:
+            getHunValue(usr_inp1)
+    except ValueError:
+        print('Error: Invalid number')
