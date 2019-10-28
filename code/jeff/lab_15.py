b9ab7e945cf258d61402702ffda1e222d5c3dd52
Jeff <jsportland@gmail.com>
2019-10-28 15:08:21 -0700

    Lab 15 --finally!

---
+++ code/jeff/lab_15.py
@@ -0,0 +1,8 @@
+#lab_15.py
+#Jeff Smith
+#Lab 15: Number to Phrase
+
+# Convert a given number into its english representation. For example: 67 becomes 'sixty-seven'. Handle numbers from 0-99.
+# Hint 2: use the digit as an index for a list of strings OR as a key for a dict of digit:phrase pairs.
+# tens_digit = num // 10
+# ones_digit = num % 10
