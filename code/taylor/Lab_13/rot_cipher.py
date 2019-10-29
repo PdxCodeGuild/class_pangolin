f0586ba642dcae03f943de05fd7b98aa41c40cef
Michael R.T <cloudleviathan@outlook.com>
2019-10-28 21:11:22 -0700

    Lab_13 Rot_v2 Complete

--- code/taylor/Lab_13/rot_cipher.py
+++ code/taylor/Lab_13/rot_cipher.py
@@ -1,31 +1,54 @@
-# A = 97 Z = 122
+# Taylor Rebbe
+# PDX Code Guild
+# Lab_13v2
+# 10/28/2019
 
-a = list(str("rambB zen").lower())
-print(a)
+# User inputs, text string and rotation number for encryptin
+usr_str1 = list(str(input("Enter some text: > ")).lower())
+usr_rot1 = int(input("Enter a rotation (1 - 18): > "))
 
-b = ['e', 'n', 'z','o','b',' ', 'm', 'r', 'a'] 
+# Initialized lists for encryption / decryption functions
+e_list = []
+d_list = []
 
-encrypted_list = []
-
-def rot_encrypt(lst, rot):
-    global encrypted_list
+# Rot encryption function, accomidates spaces 1 - 18 Rot without bugs
+def rot_encrypt(lst, rot, el):
     for i in lst:
         b = int(str(ord(i) + rot))
         while b > 122:
-            b -= 26
+            b += -122 + 96
         if b - rot == 32:
             b = 32
-        encrypted_list.append(chr(b))
-   
-rot_encrypt(a, 13)
-print(encrypted_list, "First encrypt")
-encrypted_list.clear()
+        el.append(chr(b))
 
-rot_encrypt(b, 13)
-print(encrypted_list, "Second decrypt")
+# Rot decryption function, accomidates spaces 1 - 18 Rot without bugs
+def rot_decrypt(lst, rot, dl):
+    for i in lst:
+        b = int(str(ord(i) - rot))
+        while b < 97:
+            if b + rot == 32:
+                sp = 32
+                dl.append(chr(sp))
+                break
+            c = 97 - b
+            b = 123 - c
+        dl.append(chr(b))
 
-listToStr = ''.join([str(elem) for elem in encrypted_list]) 
+# Encryption function call
+rot_encrypt(usr_str1, usr_rot1, e_list)
 
-print(listToStr)
+# Returns the encryption list to a string
+lst_to_str = ''.join([str(elem) for elem in e_list]) 
+print(lst_to_str)
 
+# User inputs, text string and rotation number for decryption
+usr_str2 = list(str(input("Enter some text: > ")).lower())
+usr_rot2 = int(input("Enter a rotation (1 - 18): > "))
+
+# Decryption function call
+rot_decrypt(usr_str2, usr_rot2, d_list)
+
+# Returns the decryption list to a string
+lst_to_str = ''.join([str(elem) for elem in d_list]) 
+print(lst_to_str)
 
