254c303685c360956eb0e1e776ec8b8b830a0a98
Michael R.T <cloudleviathan@outlook.com>
2019-10-29 10:12:42 -0700

    update

--- code/taylor/Lab_13/rot_cipher.py
+++ code/taylor/Lab_13/rot_cipher.py
@@ -14,24 +14,24 @@ d_list = []
 # Rot encryption function, accomidates spaces 1 - 25 Rot without bugs
 def rot_encrypt(lst, rot):
     for i in lst:
-        b = int(str(ord(i) + rot))
-        while b > 122:
-            b += -122 + 96
-        if b - rot == 32:
-            b = 32
-        e_list.append(chr(b))
+        x = int(str(ord(i) + rot))
+        while x > 122:
+            x += -122 + 96
+        if x - rot == 32:
+            x = 32
+        e_list.append(chr(x))
 
 # Rot decryption function, accomidates spaces 1 - 25 Rot without bugs
 def rot_decrypt(lst, rot, dl):
     for i in lst:
-        b = int(str(ord(i) - rot))
-        while b < 97:
-            if b + rot == 32:
-                b = 32
-                dl.append(chr(b))
+        x = int(str(ord(i) - rot))
+        while x < 97:
+            if x + rot == 32:
+                x = 32
+                dl.append(chr(x))
                 break
-            b += -96 + 122
-        dl.append(chr(b))
+            x += -96 + 122
+        dl.append(chr(x))
 
 # Encryption function call
 rot_encrypt(usr_str1, usr_rot1)
@@ -42,7 +42,7 @@ print(lst_to_str)
 
 # User inputs, text string and rotation number for decryption
 usr_str2 = list(str(input("Enter encoded text to decrypt: > ")).lower())
-usr_rot2 = int(input("Enter the original rotation (1 - 18): > "))
+usr_rot2 = int(input("Enter the original rotation (1 - 25): > "))
 
 # Decryption function call
 rot_decrypt(usr_str2, usr_rot2, d_list)
