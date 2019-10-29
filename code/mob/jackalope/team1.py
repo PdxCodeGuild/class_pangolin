3bda09399db0bc1282eb7617a55469957c036658
WileyRummel <wiley.rummel@gmail.com>
2019-10-29 11:43:24 -0700

    almost working

--- code/mob/jackalope/team1.py
+++ code/mob/jackalope/team1.py
@@ -7,16 +7,19 @@ year_counter = 0
 while len(jackalope_population) <= 1000:
     mate_counter = 0
     # print(jackalope_population)
-    for i in range(0,len(jackalope_population)):
-        print(f' jack is {jackalope_population[i]}')
+    for i in range(0, len(jackalope_population)):
+        print(f' jack is {(i)}')
         if jackalope_population[i] >= 4 and jackalope_population[i] <= 8:
             mate_counter += 1
             # print(f'mate counter is {mate_counter}')
-        elif jackalope_population[i] >= 10:
-            jackalope_population.pop(jackalope_population[i])
+            jackalope_population[i] += 1
+        # elif jackalope_population[i] >= 10:
+        #     jackalope_population.pop(i)
         else:
-            pass
-        jackalope_population[i] += 1
+            jackalope_population[i] += 1
+            
+        
+        #print([i])
 
     for i in range(0,mate_counter//2):
         jackalope_population.append(0)
