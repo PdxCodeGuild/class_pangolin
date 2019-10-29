90c5ba2b1eb9a3955b7e2e4ca4d40aee3673791f
Shawn Stolsig <shawn_stolsig@hotmail.com>
2019-10-29 12:33:11 -0700

    updated team 1 before lunch

--- code/mob/jackalope/team1.py
+++ code/mob/jackalope/team1.py
@@ -15,13 +15,12 @@ while len(jackalope_population) <= 1000:
         if jack >= 4 and jack <= 8:
             # increment jack counter
             mate_counter += 1
-            print(f'adding one jack to male counter.  male counter is now: {mate_counter}')
 
     # iterate through jack population and remove those who are too old
     # count how many old jacks
     old_jack_count = jackalope_population.count(10)
     print(f'removing {old_jack_count} jacks')
-    
+
     # remove that many jacks
     for i in range(0,old_jack_count):
         jackalope_population.remove(10)
@@ -30,12 +29,12 @@ while len(jackalope_population) <= 1000:
     jackalope_population = [jack + 1 for jack in jackalope_population]
         
     # add newly born jacks
+    print(f'adding {(mate_counter // 2) * 2} new babies')
     for i in range(0,mate_counter//2):
-        jackalope_population.append(0)
-        jackalope_population.append(0)
         jackalope_population.append(0)
         jackalope_population.append(0)
 
     year_counter += 1
-    print(f'end of year. there are now {len(jackalope_population)} jackalopes and it is year {year_counter}')
+    print(f'end of year. there are now {len(jackalope_population)} jackalopes and it is year counter is {year_counter}')
+    print(jackalope_population)
 
