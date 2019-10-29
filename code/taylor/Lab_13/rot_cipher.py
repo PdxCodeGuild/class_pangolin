bf10963c3f321032bd5913e41917ba89264589d6
Michael R.T <cloudleviathan@outlook.com>
2019-10-29 07:47:37 -0700

    completed lab 17

--- code/taylor/Lab_13/rot_cipher.py
+++ code/taylor/Lab_13/rot_cipher.py
@@ -5,21 +5,21 @@
 
 # User inputs, text string and rotation number for encryptin
 usr_str1 = list(str(input("Enter some text: > ")).lower())
-usr_rot1 = int(input("Enter a rotation (1 - 18): > "))
+usr_rot1 = int(input("Enter a rotation (1 - 25): > "))
 
 # Initialized lists for encryption / decryption functions
 e_list = []
 d_list = []
 
 # Rot encryption function, accomidates spaces 1 - 18 Rot without bugs
-def rot_encrypt(lst, rot, el):
+def rot_encrypt(lst, rot):
     for i in lst:
         b = int(str(ord(i) + rot))
         while b > 122:
             b += -122 + 96
         if b - rot == 32:
             b = 32
-        el.append(chr(b))
+        e_list.append(chr(b))
 
 # Rot decryption function, accomidates spaces 1 - 18 Rot without bugs
 def rot_decrypt(lst, rot, dl):
@@ -35,7 +35,7 @@ def rot_decrypt(lst, rot, dl):
         dl.append(chr(b))
 
 # Encryption function call
-rot_encrypt(usr_str1, usr_rot1, e_list)
+rot_encrypt(usr_str1, usr_rot1)
 
 # Returns the encryption list to a string
 lst_to_str = ''.join([str(i) for i in e_list]) 
