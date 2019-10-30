9e5b29141d69410766740c5ea94b0985499577c0
Michael R.T <cloudleviathan@outlook.com>
2019-10-30 15:01:35 -0700

    update

--- code/taylor/Lab_14/pick_six.py
+++ code/taylor/Lab_14/pick_six.py
@@ -5,28 +5,31 @@
 
 # Import random module
 import random
+
 # Variables
 a, b, c, d, e, f = 0, 0, 0, 0, 0, 0
+
 # Tallies ammount spent on tickets @ 2$ each
 expenses = 0
-winnings = 0
-# Loop counter / index
-i = 0
+
 # Function to calculate the winnings
 def calc_winnings(a, b, c, d, e, f):
     total = sum([a * 2, b * 4, c * 10, d * 500, e * 1000000, f * 25000000])
     return total
+
 # Function to generate a random list of 6 integers between 1 & 99 inclusive
 def get_six():
     six_lst = []
     for n in range(1, 7):
         six_lst.append(random.randint(1, 99))
     return six_lst
+
 # Main program loop
+i = 0
 while i < 100000:
     expenses += 2
     mtchs = len(set(get_six()) & set(get_six()))# set() & - intersection is used to determin matches in the set
-  # mtchs is the length of the matches list and tallied below
+
     if mtchs == 1:
         a += 1
     elif mtchs == 2:
@@ -40,6 +43,7 @@ while i < 100000:
     elif mtchs == 6:
         f += 1
     i += 1
+
 # User output
 totals = round(((calc_winnings(a, b, c, d, e, f) - expenses) / expenses) * 100)
 print("\nTotal Winnings > $",calc_winnings(a, b, c, d, e, f))
--- code/taylor/Lab_18/peaks_valleys.py
+++ code/taylor/Lab_18/peaks_valleys.py
@@ -1,4 +1,7 @@
-
+# Taylor Rebbe
+# PDX Code Guild
+# Lab_18
+# 10/30/2019
 
 
 
