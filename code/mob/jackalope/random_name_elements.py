e748e63b533ec410d2104f66cf735968f54c892b
WileyRummel <wiley.rummel@gmail.com>
2019-10-30 14:37:42 -0700

    I hope this isn't effin it up

--- code/mob/jackalope/random_name_elements.py
+++ code/mob/jackalope/random_name_elements.py
@@ -4,11 +4,11 @@ import random
 
 def random_name():
     name= ''
-    phonetic1 = ['s','t','r','ch','g','qu','th','m','n','mn','b','sh','st','w','x','z','v','h','b','j','l']
+    phonetic = ['s','t','r','ch','g','qu','th','m','n','mn','b','sh','st','w','x','z','v','h','b','j','l']
     phonetic2 = ['e','a','y','o','oo','u','ou','i','ie','ae','ee','ey']
 
     for i in range(random.randint(1,4)):
-        name += random.choice(phonetic1)+random.choice(phonetic2)
+        name += random.choice(phonetic)+random.choice(phonetic2)
     return name
 print(random_name())
     
