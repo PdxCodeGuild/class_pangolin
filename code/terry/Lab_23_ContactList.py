4b9fb612d0bb8f8b698b060534f199bcbeb2186e
terry <tlfuller@rocketmail.com>
2019-11-03 16:38:21 -0800

    Lab 23 - Contact List, separates word in string

--- code/terry/Lab_23_ContactList.py
+++ code/terry/Lab_23_ContactList.py
@@ -1,12 +1,14 @@
-
 with open('contact_list.csv', 'r') as file:
     lines = file.read().split('\n')
-    #print(lines)
+    # print(lines)
 
 contactList = []
 my_dict = {}
 
 for contact in lines:
-    print(contact)
+    my_line = contact.split(",")
+    print(my_line)
 
+    #print(contact)
 
+#print(name, fruit, color)
