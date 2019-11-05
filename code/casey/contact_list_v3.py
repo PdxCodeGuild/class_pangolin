a6dc974d361671bbd7883272b0a9819a4662ce95
Casey Burge <casey.patrick.burge@gmail.com>
2019-11-05 12:49:32 -0800

    Added Lab 23 versions 1 & 2

---
+++ code/casey/contact_list_v3.py
@@ -0,0 +1,9 @@
+'''
+Lab 23: Contact List - Version 3
+
+Purpose/goal: When REPL loop finishes:
+    
+    - Write the updated contact info to the CSV file to be saved. 
+    
+    - Save a backup contacts.csv because you likely won't write it correctly the first time.
+'''
