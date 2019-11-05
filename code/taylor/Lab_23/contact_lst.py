46f2b7f7f525db347b73079ccf501abbc97b3659
Michael R.T <cloudleviathan@outlook.com>
2019-11-04 18:56:04 -0800

    loops roughed in contact list v2

--- code/taylor/Lab_23/contact_lst.py
+++ code/taylor/Lab_23/contact_lst.py
@@ -1,14 +1,20 @@
-msg1 = "Create a contact? (Y , N): > "
-msg2 = "Find a contact? (Y , N): > "
-msg3 = "Update a contact? (Y , N): > "
-msg4 = "Delete a contact? (Y , N): > "
-
+msg1 = "Create a contact? (y , n): > "
+msg2 = "Find a contact? (y , n): > "
+msg3 = "Update a contact? (y , n): > "
+msg4 = "Delete a contact? (y , n): > "
 msg5 = "Enter a name: > "
 msg6 = "Enter a fruit: > "
 msg7 = "Enter a color: > "
+msg8 = "Enter name, fruit or color: > "
+msg9 = "Enter a value: > "
+
+emsg1 = "Check your input"
+emsg2 = "Contact not found"
+
+inp_val1 = ("y","n")
 
 with open('contacts.csv', 'r') as f:
-    csv_org = f.read().split('\n')
+    csv_orig = f.read().split('\n')
 
 # List to dictionary
 def get_dict(csvL):
@@ -22,46 +28,79 @@ def get_dict(csvL):
         csv_lst.append(dict(csv_dct))
     return csv_lst
 
-contacts = get_dict(csv_org)
+contacts = get_dict(csv_orig)
 
 # Create a record
 def new_rcd(nme, frt, clr):
     new_dct = {
-        'Name': nme,
-        'Fruit': frt,
-        'Color': clr
+        'name': nme,
+        'fruit': frt,
+        'color': clr
     }
     return contacts.append(new_dct)
-
-new_rcd('Tommy', 'orange', 'purple')
-print(contacts)
 
 # Retrieve a record
 def fnd_rcd(nme):
     for i in range(len(contacts)):
         for j in contacts[i].items():
-            if contacts[i]['Name'] == nme:
+            if contacts[i]['name'] == nme:
                 j = contacts[i]
                 return j
                
-print(fnd_rcd('Jim'))
-
 # Delete a record
 def del_rcd(nme):
     for i in range(len(contacts)):
-        if contacts[i]['Name'] == nme:
+        if contacts[i]['name'] == nme:
                 contacts.remove(contacts[i])
                 return contacts
-               
-print(del_rcd('Jim'))
 
 # Update a record
 def upd_rcd(fnd, atr, val):
     fnd[atr] = val
     upd_rcd = fnd
-    del_rcd(fnd)
+    del_rcd(fnd['name'])
     contacts.append(upd_rcd)
     return contacts
 
-print(upd_rcd(fnd_rcd('Taylor'), 'Name', 'Jimmy John'))
+def usr_inp(msg, emsg, *args):
+     while True:
+          usr_inp = input(msg).lower()
+          if usr_inp.lower() not in args:
+               print(f"\n{emsg}")
+          else:
+               return usr_inp
 
+# Create a new contact loop
+new_contact = usr_inp(msg1, emsg1, *inp_val1)
+if new_contact == 'y':
+    new_rcd(input(msg5),input(msg6),input(msg7))
+    print(contacts)
+
+# Retreive a contact loop
+retrieve_contact = usr_inp(msg2, emsg1, *inp_val1)
+if retrieve_contact == 'y':
+    record = fnd_rcd(input(msg5))
+    if record == None:
+        print(emsg2)
+    else:
+        print(record)
+
+# Update a contact loop
+update_contact = usr_inp(msg3, emsg1, *inp_val1)
+if update_contact == 'y':
+    record = fnd_rcd(input(msg5))
+    if record == None:
+        print(emsg2)
+    else:
+        upd_rcd(record, input(msg8), input(msg9))
+        print(contacts)
+
+# Delete a contact loop
+delete_contact = usr_inp(msg4, emsg1, *inp_val1)
+if delete_contact == 'y':
+    record = fnd_rcd(input(msg5))
+    if record == None:
+        print(emsg2)
+    else:
+        del_rcd(record['name'])
+        print(contacts)
