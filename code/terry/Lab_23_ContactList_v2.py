99a38bad48b29cce81f1a11237feb581cfd83a44
terry <tlfuller@rocketmail.com>
2019-11-05 19:35:58 -0800

    Lab 23 - Contact List v3, CRUD is working, need functions, write-out working, cleaned-up

--- code/terry/Lab_23_ContactList_v2.py
+++ code/terry/Lab_23_ContactList_v2.py
@@ -1,21 +1,15 @@
 with open('contact_list.csv', 'r') as file:
-    # print(list(file))
     lines = file.read().split('\n')
-    # print(lines)
 
 contactList = []
 my_dict = {}
 header = []
-# name = []
-# fruit = []
-# color = []
 my_line = []
 new_list = []
 final_list = []
 final_text = []
 
 lines = [x for x in lines if x]
-# print(lines)
 
 for contact in lines:
     my_line = contact.split(",")
@@ -27,12 +21,11 @@ new_list = zip(contactList[1], contactList[2])
 
 for i in range(1, len(contactList)):
     my_list = list(zip(contactList[0], contactList[i]))
-    # print(my_list)
+
     my_dict = dict(my_list)
-    # print(my_dict)
+
     final_list.append(my_dict)
 
-# print(final_list)
 user_choice = ""
 user_name = ""
 user_fruit = ""
@@ -55,8 +48,7 @@ elif user_choice == "f":
     for n in final_list:
         if n['Name'] == user_name:
             print(n)
-        # elif n['Name'] != user_name:
-        #     print("That user does not exist.")
+
 
 elif user_choice == "u":
     # use the .update
@@ -82,34 +74,29 @@ elif user_choice == "d":
             del my_dict['Favorite Fruit']
             del my_dict['Favorite Color']
 
-        # my_print = list(my_dict.items())
-        # print(my_print)
-
     # For testing to confirm that the Name was removed
-    for n in final_list:
-        if n['Name'] == user_name:
-            print("User not found or deleted.")
-# print(my_dict)
-# print(final_list)
+    # for n in final_list:
+    #     if n['Name'] == user_name:
+    #         print("User not found or deleted.")
+
 final_text = my_dict.keys()
 
-# print(','.join(my_dict.keys()))
+
 final_keys = []
 final_keys.append(','.join(my_dict.keys()))
 final_values = []
 for d in final_list:
     final_values.append((','.join(d.values())))
-# print(d)
+
 print(final_keys)
-# final_string = (','.join(final_values))
-# final_string.
-# Goal: ['Name,Favorite Fruit,Favorite Color\n', 'Matthew,blackberries,orange\n', 'Sam,pineapple,green\n']
+
+
 print(final_values)
 print(final_keys)
 final_string = final_keys + final_values
 final_string = str(final_string)
 
-# final_string = final_string.replace("']", "\\n")
+
 final_string = final_string.replace("['", "")
 final_string = final_string.replace("']", "")
 final_string = final_string.replace("', '", ", ")
