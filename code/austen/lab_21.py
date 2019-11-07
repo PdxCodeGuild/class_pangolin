effd9f150da40c4f5cd86dcfc2375cc3422656a1
austen <a_cote@comcast.net>
2019-11-06 20:49:22 -0800

    v1 lab21

---
+++ code/austen/lab_21.py
@@ -0,0 +1,36 @@
+import string 
+
+count = {}
+
+with open('lab21text.txt', 'r') as file:
+    #words = file.read().split('\n').lower()
+    book = file.read()
+    book = book.lower()
+    #.split('\n')
+    translator = str.maketrans('', '', string.punctuation)
+    book = book.translate(translator) # I am a string with punctuation
+    #words = str.maketrans('', '', digits)
+    #words = [i.translate(words) for i in words]
+    book = book.split()#.split('\n')
+    #print (book)
+
+    stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+
+    for word in book:
+        if word in stopwords:
+            pass
+        elif word not in count:
+            count[word] = 1
+        elif word in count:
+            count[word] += 1
+    #print(count)
+
+    words = list(count.items()) # .items() returns a list of tuples
+    words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
+    for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
+        print(words[i])
+
+
+
+
+
