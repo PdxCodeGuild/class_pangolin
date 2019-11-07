8a6cace46b9d86db81b5b08b196ff4429d8341d2
Michael R.T <cloudleviathan@outlook.com>
2019-11-07 14:53:13 -0800

    update

--- code/taylor/Lab_25/bank_class.py
+++ code/taylor/Lab_25/bank_class.py
@@ -1,33 +1,38 @@
-def BankAccount():
-    __init__
-    account = 0
-    deposit = 0
-    check_withdrawl = 0
-    withdraw = 0
+# Taylor Rebbe 
+# Lab_25
+# 11/07/19
 
-def check_balance():
-    ''' Returns the account balance '''
+class BankAccount():
+    def __init__(self):
+        
+        account = 0,
+        deposit = 0,
+        check_withdrawl = 0,
+        withdraw = 0
 
-def deposit(amount):
-    ''' Deposits the given amount in the account '''
+    def deposit(self, amount):
+        ''' Deposits the given amount in the account '''
 
-def check_withdrawl(amount):
-    ''' Returns true if hte withdrawn amount won't put the account in the negative '''
+    def check_balance(self):
+        ''' Returns the account balance '''
 
-def withdraw(amount):
-    ''' WIthdraws the amount from hte account and returns it ''' 
+    def check_withdrawl(self, amount):
+        ''' Returns true if hte withdrawn amount won't put the account in the negative '''
 
-# Version 2
-''' Have the ATM maintain a list of transactions. Every time the user makes a deposit or withdrawal, add a string to a list saying 'user deposited $15' or 'user withdrew $15'. Add a new function print_transactions() to your class for printing out the list of transactions '''
+    def withdraw(self, amount):
+        ''' WIthdraws the amount from hte account and returns it ''' 
+
+    # Version 2
+    ''' Have the ATM maintain a list of transactions. Every time the user makes a deposit or withdrawal, add a string to a list saying 'user deposited $15' or 'user withdrew $15'. Add a new function print_transactions() to your class for printing out the list of transactions '''
 
 
-# Version 3
-''' Allow the user to enter commands into a REPL.
+    # Version 3
+    ''' Allow the user to enter commands into a REPL.
 
-> what would you like to do (deposit, withdraw, check balance, history)?
-> deposit
-> how much would you like to deposit?
-> $5
-> what would you like to do (deposit, withdraw, check balance, history)?
-> check balance
-> balance: $5 '''
+    > what would you like to do (deposit, withdraw, check balance, history)?
+    > deposit
+    > how much would you like to deposit?
+    > $5
+    > what would you like to do (deposit, withdraw, check balance, history)?
+    > check balance
+    > balance: $5 '''
