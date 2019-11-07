27887840244b5ad8106875c3b27bc0c9b1ef706c
Michael R.T <cloudleviathan@outlook.com>
2019-11-07 15:52:07 -0800

    update

--- code/taylor/Lab_25/bank_class.py
+++ code/taylor/Lab_25/bank_class.py
@@ -6,7 +6,7 @@ class BankAccount():
 
     def __init__(self, account=0):
         self.account = account
-
+ 
     def deposit(self, amount=0):
         ''' Deposits the given amount in the account '''
         self.account += amount
@@ -16,16 +16,15 @@ class BankAccount():
         ''' Returns the account balance '''
         return self.account
 
+    def withdraw(self, amount):
+        ''' WIthdraws the amount from the account and returns it '''
+        self.account - amount
+        return self.account
 
     def check_withdraw(self, amount=0):
-        ''' Returns true if hte withdrawn amount won't put the account in the negative '''
+        ''' Returns true if the withdrawn amount won't put the account in the negative '''
         if self.account - amount >= 0:
-            print('Oki-Doki')
-        else:
-            print('Over Draft!')
-
-    def withdraw(self, amount=0):
-        ''' WIthdraws the amount from hte account and returns it ''' 
+            return True
 
     # Version 2
     ''' Have the ATM maintain a list of transactions. Every time the user makes a deposit or withdrawal, add a string to a list saying 'user deposited $15' or 'user withdrew $15'. Add a new function print_transactions() to your class for printing out the list of transactions '''
@@ -42,9 +41,9 @@ class BankAccount():
     > check balance
     > balance: $5 '''
 
-taco = BankAccount()
-taco.deposit(155.22)
-print(taco.account)
-taco.deposit(4.78)
-print(taco.check_balance())
-taco.check_withdraw(150.00)
+taco = BankAccount(155)
+a = taco.check_withdraw(10)
+if a == True:
+    print('True')
+else:
+    print('False')
