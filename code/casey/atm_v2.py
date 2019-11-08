8023b3a294c8a3e9638f5ffa5a7473ccede64a11
Casey Burge <casey.patrick.burge@gmail.com>
2019-11-08 11:47:10 -0800

    updated

---
+++ code/casey/atm_v2.py
@@ -0,0 +1,10 @@
+'''
+Lab 25: ATM - Version 2
+
+Purpose/goal: Have the ATM maintain a list of transactions. 
+
+    - Every time the user makes a deposit or withdrawal, add a string to    a list saying 'user deposited $15' or 'user withdrew $15'. 
+    
+    - Add a new function print_transactions() to your class for printing    out the list of transactions.
+
+'''
