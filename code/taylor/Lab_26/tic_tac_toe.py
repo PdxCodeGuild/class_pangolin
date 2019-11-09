8d274b8e855a1bd6801f99adefdd664e9605ce69
Michael R.T <cloudleviathan@outlook.com>
2019-11-08 16:05:23 -0800

    update

---
+++ code/taylor/Lab_26/tic_tac_toe.py
@@ -0,0 +1,18 @@
+# Tic Tac Toe User Messages and Display
+# Thank you for visiting https://asciiart.website/
+# This ASCII pic can be found at
+# https://asciiart.website/index.php?art=objects/computers
+
+a,b,c,d,e,f,g,h,i = '_','_','_','_','_','_',' ',' ',' '
+
+print(f'''
+       ._,-,_.              _    ________    _       ______    __
+       ||| |||    {a}|{b}|{c}    / \  |__    __|  / \     |   _  \  |  |
+       ||| |||    {d}|{e}|{f}   / . \    |  |    / . \    |  |_) /  |  |
+       ;|| ||:    {g}|{h}|{i}  / /_\ \   |  |   / /_\ \   |     (   |  |
+     ./ /| |\ \.        /  ___  \  |  |  /  ___  \  |  |\  \  |  |
+     |./ :_: \.|       /__/   \__\ |__| /__/   \__\ |__| \__\ |__|''')
+
+
+
+
