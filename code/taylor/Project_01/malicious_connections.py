f787e77bebbd23cba5d9f948a3bffbb994766f5a
Michael R.T <cloudleviathan@outlook.com>
2019-11-13 14:47:49 -0800

    dict object manipulation

--- code/taylor/Project_01/malicious_connections.py
+++ code/taylor/Project_01/malicious_connections.py
@@ -1,7 +1,4 @@
-import psutil
-import config
-import requests
-
+import psutil, config, requests, time, json, pprint
 
 
 fetch_remote_system_connections = list(psutil.net_connections(kind='inet4'))
@@ -16,11 +13,13 @@ def remote_connections(connections):
 # Variable stores the remote connections (k, v) tupples as a dictionary
 remote_connections_dict = remote_connections(fetch_remote_system_connections)
 
-print(remote_connections_dict)
-
-# Virus total
+#
 url = 'https://www.virustotal.com/vtapi/v2/ip-address/report'
-params = {'apikey': '<apiToken>', 'ip':'<ipaddress>'}
-response = requests.get(url, params=params)
+params = {'apikey': config.api_key, 'ip':'52.141.217.125'}
+# response = requests.get(url, params=params)
+response = requests.get(url, params=params).json()
 
-print(response.json())
+# a = response.json()
+
+pprint.pprint((response['as_owner']))
+
