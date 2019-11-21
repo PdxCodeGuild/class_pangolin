287c52d2f17155a2e5577a649706f42f1b80cc72
Jeff <jsportland@gmail.com>
2019-11-21 10:36:02 -0800

    fixing merge problem

--- code/jeff/weather.py
+++ code/jeff/weather.py
@@ -1,6 +1,8 @@
-# weather.py
+# python_final.py
+# weather update
 # Python final project
 # Jeff Smith
+
 import string
 import requests
 from pprint import pprint
@@ -11,53 +13,54 @@ from email.mime.multipart import MIMEMultipart
 # OpenWeatherMap API key
 APIKEY = '248f86c4f28e22722ba6efee4a3066db'
 
-# Search criteria pulling current +- 10min, for Portland, OR. Return 'Imperial' results.
+# Search criteria pulling current (<10min) for Portland, OR. Return 'Imperial' results. 
+# Future version will be searchable for any city.
+
 url = 'http://api.openweathermap.org/data/2.5/forecast?id=4720131&APPID=248f86c4f28e22722ba6efee4a3066db&units=imperial'
 
 res = requests.get(url) # Retrieve the page
 data = res.json() # Put the local data into a searchable format.
-#pprint(data)
-# Allows you to visually identify the location of your target data.
-# 
+
+# pprint(data) # Allows you to visually identify the location of your target data.
+
+# Specify selected data points
 description = data['list'][0]['weather'][0]['description']
 wind_speed = data['list'][0]['wind']['speed']
 temp = data['list'][0]['main']['temp']
 humid = data['list'][0]['main']['humidity']
 
-# def body():
-    # create 'body' of outgoing message
-description = data['list'][0]['weather'][0]['description']
-wind_speed = data['list'][0]['wind']['speed']
-temp = data['list'][0]['main']['temp']
-humid = data['list'][0]['main']['humidity']
-
-c = '\033[4mCurrent Conditions\033[0m:\n'
-t = 'Temperature : {}\u00B0\n'.format(temp)
-w = 'Wind : {} mph\n'.format(wind_speed)
-d = 'Description : {}\n'.format(description)
-h = 'Humidity: {} %\n'.format(humid)
+ # create 'body' of outgoing message
+c = 'Current Conditions : {}'.format(description)
+t = 'Temperature : {} degrees'.format(temp)
+w = 'Wind : {} mph'.format(wind_speed)
+h = 'Humidity: {} %'.format(humid)
+# c = '\033[4mCurrent Conditions\033[0m: {}\n'.format(description)\n'
+# t = 'Temperature : {}\u00B0\n'.format(temp)
+# w = 'Wind : {} mph\n'.format(wind_speed)
+# h = 'Humidity: {} %\n'.format(humid)
     
-    # return c + t + w + d + h
-
-# print(body())
 email = "pdx1tail@gmail.com"
 pas = "liwvpzwnkmohbvdj"
 to = 'jsportland@gmail.com'
 # sms_gateway = '15035011958@vtext.com'
-
-smtp = "smtp.gmail.com" #outgoing server for sms
+smtp = "smtp.gmail.com"
 port = 587
-server = smtplib.SMTP(smtp,port) # initialize smtp server
-server.starttls()# Starting the server
-sl = server.login(email,pas)# Pass login info
-#print(server.login(email,pas))# Pass login info
-msg = MIMEMultipart()# Use the MIME module to structure our message.
+server = smtplib.SMTP(smtp,port) 
+server.starttls() # Starting the server
+sl = server.login(email,pas) # Pass login info
+# print(server.login(email,pas)) # In the event of mail login issues
+msg = MIMEMultipart()
 msg['From'] = email
 msg['To'] = to
-msg['Subject'] = f"Weather Report\n\n{c + t + w + d + h}" 
-#msg['body'] = body
-msg.attach(MIMEText(msg['Subject'], 'plain'))
-server.sendmail(msg['From'], msg['To'], msg['Subject'])
+msg['Subject'] = f"Weather Report{c + t + w + h}" 
+# msg['Subject'] = "Weather Report\n\n" 
+body = f"Weather Report\n\n{c, t, w, h}\n"
+msg.attach(MIMEText(body, 'plain'))
+server.sendmail(msg['From'], msg['To'], msg.as_string())
 
-# # lastly quit the server
+# Quit the SMTP server
 server.quit()
+
+# Future additions to include: 
+# 1) SMS to replace email 2)Search for any city 3)choice of target data 
+# 4)daily update/hourly check for triggers 5)trigger alert
