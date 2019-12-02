c5bc2c9f7a83772d914e5e8cea20e6e34651ecd0
terry <tlfuller@rocketmail.com>
2019-12-02 15:04:48 -0800

    update

--- code/terry/polls_tutorial/polls/views.py
+++ code/terry/polls_tutorial/polls/views.py
@@ -3,6 +3,7 @@ from django.http import HttpResponseRedirect
 from .models import Question, Choice
 from django.urls import reverse
 from django.views import generic
+from django.utils import timezone
 
 # Create your views here.
 # def index(request):
@@ -25,7 +26,7 @@ class IndexView(generic.ListView):
     context_object_name = "latest_question_list"
 
     def get_queryset(self):
-        return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[:5]
 
 class DetailView(generic.DetailView):
     model = Question
