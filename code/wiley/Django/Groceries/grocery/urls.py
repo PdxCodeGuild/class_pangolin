b0b765938b939b09dd4d986e6c5118eb4a167235
WileyRummel <wiley.rummel@gmail.com>
2019-12-04 14:09:22 -0800

    Grocery List

---
+++ code/wiley/Django/Groceries/grocery/urls.py
@@ -0,0 +1,27 @@
+from django.urls import path, include
+from django.contrib import admin
+from grocery_list import views
+
+
+"""grocery URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.0/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+
+app_name = 'grocery_list'
+urlpatterns = [
+    path('grocery/', include('grocery_list.urls')),
+    path('admin/', admin.site.urls),
+    
+]
---
+++ code/wiley/Django/Groceries/grocery_list/views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse, HttpResponseRedirect
+from django.views import generic
+from django.urls import reverse
+from django.utils import timezone
+from .models import GroceryItem
+
+
+
+# Create your views here.
+# class IndexView(generic.ListView):
+#     template_name = 'grocery_list.html'
+#     context_object_name = 'items'
+
+#     def get_queryset(self):
+#         return GroceryItem.objects.order_by('-date_created')
+def index(request):
+    items = GroceryItem.objects.order_by('-date_created')
+    context = {
+       'items': items,
+    }
+    return render(request, 'grocery_list/index.html', context)
+
+def item(request, GroceryItem_id):
+    grocery_item = get_object_or_404(GroceryItem,pk=GroceryItem_id)
+
+def add_item(request):
+    item_name = request.POST["add_item"]
+    GroceryItem.objects.create(text_description=item_name, date_created=timezone.now())
+    return HttpResponseRedirect(reverse('grocery_list:index'))
+
+def purchased(request):
+    grocery_item = get_object_or_404(GroceryItem, pk=request.POST["purchased"])
+    if grocery_item.completed == False:
+        grocery_item.completed = True
+        grocery_item.date_completed = timezone.now()
+    elif grocery_item.completed == True:
+        grocery_item.completed = False
+    grocery_item.save()
+
+    return HttpResponseRedirect(reverse('grocery_list:index'))
+
+def delete(request):
+    completed_items = GroceryItem.objects.filter(completed=True)
+    print(completed_items)  
+    for completed_item in completed_items:
+        completed_item.delete()  
+    return HttpResponseRedirect(reverse('grocery_list:index'))
+    
+
+    
