c78c996a6450823bf1517b9778173b286083358d
franki <franki.wangen@gmail.com>
2019-12-04 16:35:02 -0800

    finished grocery list

--- code/franki/django_lab_1/grocery/grocery/urls.py
+++ code/franki/django_lab_1/grocery/grocery/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from grocery_app.views import listView, addItem, deleteItem, completeItem
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('grocery_app/', listView),
