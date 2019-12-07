b73fb6ffbeb69b6bc3379e461ba72e6edb3223e7
Shawn Stolsig <shawnstolsig@gmail.com>
2019-12-06 17:20:46 -0800

    changed database to store all words

--- code/shawn/django/madlib-project/madlibapp/models.py
+++ code/shawn/django/madlib-project/madlibapp/models.py
@@ -3,31 +3,15 @@ from django.db import models
 # MadLib will hold title and be one-to-many with MadLibWord
 class MadLib(models.Model):
     title = models.CharField(max_length=200)
-    text = models.TextField()                                           # the text field containing madlib text plus MadLibWords
 
     # a helper function to parse the text 
-    def parse_text(self):
+    def parse_text(self, input_string):
 
-        # split input on ***
-        self.text = self.text.split('***')
-
-        # iterate through input string
-        for item in self.text:
-            # if a 'noun' is found
-            if item in ['noun','adjective','verb','plural noun','pronoun','adverb']:
-                # create new MadLiBWord 
-                item = MadLibWord.objects.create(word_type=item, madlib=self, word='')
-
-        # save changes
-        self.save()
-
-
+        # iterate through input string and create MadLibWords
+        for list_word in input_string:
+            MadLibWord.objects.create(madlib=self, word=list_word)
 
 # MadLibWord will be many-to-one with MadLib.  Each MadLibWord has the word, type of word, and ForeignKey to the MadLib puzzle to which it belongs
 class MadLibWord(models.Model):
     word = models.CharField(max_length=50, blank=True, null=True)                              # limit to max of 50 characters
-    word_type = models.CharField(max_length=20)                         # valid word types: 'noun', 'verb', 'adjective', 'plural noun', 'adverb', 'pronoun'
     madlib = models.ForeignKey(MadLib, on_delete=models.CASCADE)
-    
-    def is_madlibword(self):
-        return True
--- code/shawn/django/madlib-project/madlibapp/views.py
+++ code/shawn/django/madlib-project/madlibapp/views.py
@@ -16,9 +16,19 @@ def edit(request, madlib_id):
 
     # check to see if creating new madlib, in which case madlab_id would be 0
     if madlib_id != 0:
+
+        # get the madlib
+        ml = get_object_or_404(MadLib, pk=madlib_id)
+
+        # get all the madlibwords and turn into a presentable string
+        pretty_madlibwords = ''
+        for word in MadLibWord.objects.filter(madlib=ml):
+            pretty_madlibwords += word.word + " "
+
         # get input MadLib from db, load into context
-        context = { 'this_madlib': get_object_or_404(MadLib, pk=madlib_id), 
-                    'this_madlib_id': madlib_id}
+        context = { 'this_madlib': ml, 
+                    'this_madlib_id': madlib_id, 
+                    'this_madlibwords': pretty_madlibwords}
 
     # empty context if starting new
     else: 
@@ -33,26 +43,25 @@ def hidden_edit(request, madlib_id):
     # get the input title
     input_title = request.POST['madlibtitle']
 
-    # get input text
-    input_string = request.POST['madlibtextarea']
+    # get input text and split into list on " ".  Currently, stripping out all newlines as well
+    input_string = request.POST['madlibtextarea'].replace('\n',' ').split(' ')
 
     # if madlib_id is zero, create new madlib
     if madlib_id == 0:
         # create new MadLib
-        m = MadLib.objects.create(title=input_title, text=input_string)
+        m = MadLib.objects.create(title=input_title)
         # parse the input text to populate with MadLibWords
-        m.parse_text()
+        m.parse_text(input_string)
 
     # else, edit existing MadLib in the db
     else:
         # get madlib, update title/text, then save
         this_madlib = get_object_or_404(MadLib, pk=madlib_id)
-        this_madlib.text = input_string
         this_madlib.title = input_title
         this_madlib.save()
 
         # parse the input text to populate with MadLibWords
-        this_madlib.parse_text()
+        this_madlib.parse_text(input_string)
 
     return HttpResponseRedirect(reverse('madlibapp:index'))
 
@@ -73,9 +82,18 @@ def play(request, madlib_id):
     # get this MadLib. store as variable so that it's MadLibWords can also be retrieved
     this_madlib = MadLib.objects.get(pk=madlib_id)
 
+    # get a list of the madlib words
+    madlibwords_list = []
+    for word in MadLibWord.objects.filter(madlib=this_madlib):
+        try:
+            if word.word[0] == '*':
+                madlibwords_list.append(word.word[1:])
+        except IndexError:
+            continue
+
     # get input MadLib and all it's MadLibWords from db, load into context
-    context = { 'this_madlib': this_madlib } 
-                # 'this_madlib_words': MadLibWord.objects.filter(madlib=this_madlib)}
+    context = { 'this_madlib': this_madlib,
+                'madlibwords': madlibwords_list } 
 
     # render edit.html template
     return render(request, 'madlibapp/play.html', context)
