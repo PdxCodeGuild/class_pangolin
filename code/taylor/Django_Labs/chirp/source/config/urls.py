34208765bb1e8a3cef9018af9088fc7f789227e6
clouddenizen <tr@clouddenizen.com>
2019-12-13 11:47:51 -0800

    base

--- code/taylor/Django_Labs/chirp/source/config/urls.py
+++ code/taylor/Django_Labs/chirp/source/config/urls.py
@@ -18,6 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('', include('posts.urls')),
-    path('', include('users.urls')),
+    path('users/', include('users.urls')),
     path('admin/', admin.site.urls),
 ]
