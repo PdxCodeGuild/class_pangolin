e5ccad375c98184f26b46f07b14424acb4ddcf3a
business <business@businesss-MBP.localdomain>
2019-12-13 15:51:44 -0800

    update

--- code/terry/Lab_03_Chirp_Project/chirp_project/chirpApp/models.py
+++ code/terry/Lab_03_Chirp_Project/chirp_project/chirpApp/models.py
@@ -4,7 +4,7 @@ from django.urls import reverse
 class Chirp(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
-    body_text = models.TextField(max_length=128)
+    body_text = models.TextField('Chirp', max_length=128)
 
     def __str__(self):
         return self.author
