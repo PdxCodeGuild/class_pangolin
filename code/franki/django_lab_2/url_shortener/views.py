5549ac7dfd423fc245e0c9a71b1df29808587906
franki <franki.wangen@gmail.com>
2019-12-13 14:58:38 -0800

    completed lab2

--- code/franki/django_lab_2/url_shortener/views.py
+++ code/franki/django_lab_2/url_shortener/views.py
@@ -1,23 +1,25 @@
-from django.shortcuts import render
-import random
-import string
-from django.http import HttpResponseRedirect
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import ShortUrl
+from .forms import  UrlForm
+from .shortener import Shortener
 
-def homeView(request):
-    # try:
-    #     error = request.GET['error']
-    # except:
-    #     error = None
-    # context = {
-    #     'error': error,
-    #     }
-    return render(request, 'home.html', context)
 
-def submitUrl(request):
-    code = ''.join([random.choice(string.ascii_letters + string.digits) for n in range(6)])
-    return HttpResponseRedirect
 
-def redirectUser(request, code):
-    url = Abbreviation.objects.get(code) 
-    return HttpResponseRedirect(url)
+def Make(request):
+    form = UrlForm(request.POST)
+    a = ""
+    if request.method == 'POST':
+        if form.is_valid():
+            NewUrl = form.save(commit=False)
+            a = Shortener().issue_token()
+            NewUrl.short_url = a
+            NewUrl.save()
+        else:
+            form = UrlForm()
+            a = "Invalid URL"
+    return render(request, 'home.html', {'form': form, 'a': a})
 
+def Home(request, token):
+    long_url = ShortUrl.objects.filter(short_url=token)[0]
+    return redirect(long_url.long_url)
