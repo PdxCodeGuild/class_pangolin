4d5c86f5c797ffa4ed5a90f3e246579648d78112
franki <franki.wangen@gmail.com>
2019-12-13 11:51:01 -0800

    urls

---
+++ code/franki/django_lab_2/url_shortener/admin.py
@@ -0,0 +1,4 @@
+from django.contrib import admin
+from url_shortener.models import Abbreviation
+
+admin.site.register(Abbreviation)
--- code/franki/lab_2_draft/shorten/url_shortener/admin.py
+++ code/franki/lab_2_draft/shorten/url_shortener/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Abbreviation
+from url_shortener.models import Abbreviation
 
 admin.site.register(Abbreviation)
 # Register your models here.
