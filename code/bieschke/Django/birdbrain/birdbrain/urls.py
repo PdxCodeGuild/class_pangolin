37e04e1e8bad6515f4569f27cd6ac21587c82535
Brandon Bieschke <mistahbieschke@gmail.com>
2019-12-13 16:25:30 -0800

    manage.py

--- code/bieschke/Django/birdbrain/birdbrain/urls.py
+++ code/bieschke/Django/birdbrain/birdbrain/urls.py
@@ -1,4 +1,4 @@
-"""blog_project URL Configuration
+"""birdbrain URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -14,11 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', include('django.contrib.auth.urls')),
-    path('user/', include('users.urls')),
     path('', include('posts.urls')),
-]
+    path('', include('django.contrib.auth.urls')),
+    path('', include('users.urls')),
+
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- code/bieschke/Django/shortener/shortener_app/views.py
+++ code/bieschke/Django/shortener/shortener_app/views.py
@@ -5,7 +5,10 @@ from django.utils import timezone
 from django.views import generic
 
 from .models import Links
-from django.http import HttpResponse
+
+def redirect_view(request):
+    return HttpResponse("Hello!")
+
 
 def index(request):
     links = Links.objects.filter(created_date__lte=timezone.now()).order_by('created_date')
@@ -17,8 +20,19 @@ def shorten(request):
     return HttpResponseRedirect(reverse('shorten_app:index'))
 
 
-def lengthen(request):
+def redirect(request):
     lengthen_link = models.Links.lengthen.link
     return HttpResponseRedirect(reverse('shorten_app:index'))
 
-    
+'''
+from django.shortcuts import redirect, get_object_or_404
+
+from shortlinks.models import Links
+from shortlinks.utls import update_request_count_task
+
+def link(request, id):
+    db_id = Links.decode_id(id)
+    link_db = get_object_or_404(Links, id=db_id, active=True, status=True)
+    update_request_count_task.(db_id)
+    return redirect(link_db.link)
+'''
