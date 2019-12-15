9afb2db09b9601a425e690e677e66b8f7ef88d5d
clouddenizen <tr@clouddenizen.com>
2019-12-15 01:06:40 -0800

    posts

--- code/taylor/Django_Labs/chirp/source/posts/urls.py
+++ code/taylor/Django_Labs/chirp/source/posts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path 
+from .views import PostListView, PostCreateView
 from . import views
 from users import views as user_views
 
 urlpatterns = [
   
-  path('', views.index, name='index'),
+  path('', PostListView.as_view(), name='index'),
+  path('users/', PostCreateView.as_view(), name='post-create'),
  
 ]
--- code/taylor/Django_Labs/chirp/source/posts/views.py
+++ code/taylor/Django_Labs/chirp/source/posts/views.py
@@ -1,11 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
 
-# Create your views here.
-def index(request):
-  context = {
-    'posts': Post.objects.all()
-    }
-  return render(request, 'posts/index.html', context)
 
+# def index(request):
+#   context = {
+#     'posts': Post.objects.all()
+#     }
+#   return render(request, 'posts/index.html', context)
+
+class PostListView(ListView):
+  model = Post
+  template_name = 'posts/index.html'
+  context_object_name = 'posts'
+  ordering = ['-date_posted']
+
+class PostCreateView(CreateView):
+  model = Post
+  template_name = 'users/users.html'
+  fields = ['title', 'content']
+  success_url = '/'
+
+  def form_valid(self, form):
+    form.instance.author = self.request.user
+    return super().form_valid(form)
