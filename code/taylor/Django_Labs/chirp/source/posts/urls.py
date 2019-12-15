a95e764fe91d14ee57dff3499dbaf8755df7bf01
clouddenizen <tr@clouddenizen.com>
2019-12-15 13:09:04 -0800

    chirp_mvp_compltet

--- code/taylor/Django_Labs/chirp/source/posts/urls.py
+++ code/taylor/Django_Labs/chirp/source/posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path 
-from .views import PostListView, PostCreateView
+from .views import PostListView, PostCreateView, UserPostListView
 from . import views
 from users import views as user_views
 
@@ -7,5 +7,6 @@ urlpatterns = [
   
   path('', PostListView.as_view(), name='index'),
   path('users/', PostCreateView.as_view(), name='post-create'),
+  path('users/<str:username>', UserPostListView.as_view(), name='user-post'),
  
 ]
--- code/taylor/Django_Labs/chirp/source/posts/views.py
+++ code/taylor/Django_Labs/chirp/source/posts/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 from django.views.generic import ListView, DetailView, CreateView
 from .models import Post
+from django.contrib.auth.models import User
 
 
 # def index(request):
@@ -15,6 +16,19 @@ class PostListView(ListView):
   template_name = 'posts/index.html'
   context_object_name = 'posts'
   ordering = ['-date_posted']
+  paginate_by = 6
+
+class UserPostListView(ListView):
+  model = Post
+  template_name = 'posts/user_posts.html'
+  context_object_name = 'posts'
+
+  paginate_by = 6
+
+  def get_queryset(self):
+    user = get_object_or_404(User, username=self.kwargs.get('username'))
+    return Post.objects.filter(author=user).order_by('-date_posted')
+
 
 class PostCreateView(CreateView):
   model = Post
