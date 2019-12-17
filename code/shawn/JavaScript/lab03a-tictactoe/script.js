b5b0086ca4d850764449b5be90cab6f8131b9f7c
Shawn Stolsig <shawnstolsig@gmail.com>
2019-12-17 12:33:27 -0800

    completed GUI

--- code/shawn/JavaScript/lab03a-tictactoe/script.js
+++ code/shawn/JavaScript/lab03a-tictactoe/script.js
@@ -2,6 +2,7 @@ class Player{
     constructor(input_name, input_symbol){
         this.name = input_name;
         this.token = input_symbol;
+        this.wins = 0;
     }
 }
 class Game{
@@ -10,36 +11,6 @@ class Game{
                          [' ',' ',' '],
                          [' ',' ',' ']];
     }
-
-
-    printBoard(){
-        let print_str = "*********** tic tac toe ***********\n";
-
-        for (let y = 0; y < this.board.length; y++){
-            for (let x = 0; x < this.board[y].length; x++){
-
-                // for manipulating DOM
-                let cellCoord = `c${x}${y}`;
-                let cell = document.getElementById(cellCoord);
-                cell.innerText = this.board[y][x];
-
-                // for printing text version
-                if (x == 1){
-                    print_str += `| ${this.board[y][x]} |`;
-                }
-                else {
-                    print_str += ` ${this.board[y][x]} `;
-
-                }
-            }
-            print_str += '\n';
-        }
-        print_str += "***********************************"
-        console.log(print_str);
-        alert(print_str);
-    }
-
-
 
     calcWinner(){
         let symbols = ['X', 'O'];
@@ -62,7 +33,6 @@ class Game{
             else if (this.board[0][2] == symbol && this.board[1][2] == symbol && this.board[2][2] == symbol){
                 return symbol;
             }
-
             else if (this.board[0][0] == symbol && this.board[1][1] == symbol && this.board[2][2] == symbol){
                 return symbol;
             }
@@ -83,53 +53,27 @@ class Game{
         return true;
     }
 
-    isGameOver(){
-        if (this.calcWinner() || this.isFull()){
-            return true;
-        }
-        return false;
-    }
-
-    getValidXYCoord(player){
-        while(true){
-            
-            let user_input = prompt(`${player.name}'s turn.  Please input x,y coordinates for ${player.token} symbol: `);
-
-            let x_input = user_input[0];
-            let y_input = user_input[2];
-            if(this.board[y_input][x_input] === ' '){
-                return [x_input, y_input]
-            } else {
-                alert(`${x_input},${y_input} already taken!  Please enter new coordinates.`)
-            }
-        }
-    }
-
-    move(x,y,player_token){
-        this.board[y][x] = player_token;
-    }
-    
     playTurn(player, coord){
         // add that player's token to the game board logic
         this.board[coord[2]][coord[1]] = player.token;
         // add visual representation of token.  remove button.
-        document.getElementById(coord).innerHTML = player.token;
+        document.getElementById(coord).innerHTML = `<img src="player${player.token}.png"/>`;
     }
 }
 
-
-
 function gameInit(){
-    userName1 = prompt("Enter player 1 name: ");
-    userName2 = prompt("Enter player 2 name: ");
-    let player1 = new Player(userName1, 'X');
-    let player2 = new Player(userName2, 'O');
+    let player1 = new Player(document.getElementById('player1-name').value, 'X');
+    let player2 = new Player(document.getElementById('player2-name').value, 'O');
     let gameBoard = new Game();
 
     return [gameBoard, player1, player2];
 }
 
 function go(cellCoord){
+
+    //update player names
+    player1.name = document.getElementById('player1-name').value
+    player2.name = document.getElementById('player2-name').value
 
     if(counter % 2 == 1){
         var player = player1;
@@ -140,25 +84,73 @@ function go(cellCoord){
     }
 
     gameBoard.playTurn(player, cellCoord);
-    // gameBoard.printBoard();
-
-    if(gameBoard.calcWinner()){
-        document.getElementById('message').innerText = `Player ${gameBoard.calcWinner()} wins!`;
-        let buttons = document.getElementsByTagName('button');
-        for (let button of buttons){
-            button.disabled = true;
+    let winnerChar = gameBoard.calcWinner();
+    if(winnerChar){
+        let winnerName = '';
+        if(winnerChar == 'X') {
+            winnerName = player1.name;
+            player1.wins++;
+            document.getElementById('player1-wins').innerText = player1.wins;
+        } 
+        else {
+            winnerName = player2.name;
+            player2.wins++;
+            document.getElementById('player1-wins').innerText = player1.wins;
         }
-        document.getElementById('play-again').innerHTML = '<br><button onclick=location.reload()>Play again?</button>';
+        document.getElementById('message').innerText = `${winnerName} wins!`;
+        let buttons = document.querySelectorAll('.cell-button');
+        for (let button of buttons){
+            button.remove();
+        }
+        document.getElementById('play-again').innerHTML = '<br><button onclick=newGame() class="play-again">Play again?</button>';
 
     } else if (gameBoard.isFull()){
-        document.getElementById('message').innerText = `The board is full...players tie.`;
-        document.getElementById('play-again').innerHTML = '<br><button onclick=location.reload()>Play again?</button>';
+        document.getElementById('message').innerText = `The board is full...tie.`;
+        document.getElementById('play-again').innerHTML = '<br><button onclick=newGame() class="play-again">Play again?</button>';
     } else {
         counter++;
         document.getElementById('message').innerText = `${nextPlayer.name}'s turn.  Click a button to place your ${nextPlayer.token} symbol: `
     }
 }
 
+// function for starting new game
+function newGame(){
+    start = gameInit();
+    gameBoard = start[0];
+    counter = 1;
+    document.getElementById('message').innerText = `${player1.name}'s turn.  Click a button to place your ${player1.token} symbol: `
+    document.querySelector(".grid").innerHTML = `
+    <div class='grid'>
+        <div id="c00">
+            <button onclick="go('c00')" class="cell-button">Play Here!</button>
+        </div>
+        <div id="c10">
+            <button onclick="go('c10')" class="cell-button">Play Here!</button>
+        </div>
+        <div id="c20">
+            <button onclick="go('c20')" class="cell-button">Play Here!</button>
+        </div>
+        <div id="c01">
+            <button onclick="go('c01')" class="cell-button">Play Here!</button>
+        </div>
+        <div id="c11">
+            <button onclick="go('c11')" class="cell-button">Play Here!</button>
+        </div>
+        <div id="c21">
+            <button onclick="go('c21')" class="cell-button">Play Here!</button>
+        </div>
+        <div id="c02">
+            <button onclick="go('c02')" class="cell-button">Play Here!</button>
+        </div>
+        <div id="c12">
+            <button onclick="go('c12')" class="cell-button">Play Here!</button>
+        </div>
+        <div id="c22">
+            <button onclick="go('c22')" class="cell-button">Play Here!</button>
+        </div>
+    </div>`
+}
+//manually setup first game
 let start = gameInit();
 let gameBoard = start[0];
 let player1 = start[1];
@@ -166,144 +158,4 @@ let player2 = start[2];
 var counter = 1;
 document.getElementById('message').innerText = `${player1.name}'s turn.  Click a button to place your ${player1.token} symbol: `
 
-// class Game{
-//     constructor(){
-//         this.board =    [[' ',' ',' '],
-//                          [' ',' ',' '],
-//                          [' ',' ',' ']];
-//     }
 
-
-//     printBoard(){
-//         let print_str = "*********** tic tac toe ***********\n";
-
-//         for (let y = 0; y < this.board.length; y++){
-//             for (let x = 0; x < this.board[y].length; x++){
-
-//                 // for manipulating DOM
-//                 let cellCoord = `c${x}${y}`;
-//                 let cell = document.getElementById(cellCoord);
-//                 cell.innerText = this.board[y][x];
-
-//                 // for printing text version
-//                 if (x == 1){
-//                     print_str += `| ${this.board[y][x]} |`;
-//                 }
-//                 else {
-//                     print_str += ` ${this.board[y][x]} `;
-
-//                 }
-//             }
-//             print_str += '\n';
-//         }
-//         print_str += "***********************************"
-//         console.log(print_str);
-//         alert(print_str);
-//     }
-
-//     move(x,y,player_token){
-//         this.board[y][x] = player_token;
-//     }
-
-//     calcWinner(){
-//         let symbols = ['X', 'O'];
-//         for (let symbol of symbols){
-//             if (this.board[0][0] == symbol && this.board[0][1] == symbol && this.board[0][2] == symbol){
-//                 return symbol;
-//             }
-//             else if (this.board[1][0] == symbol && this.board[1][1] == symbol && this.board[1][2] == symbol){
-//                 return symbol;
-//             }
-//             else if (this.board[2][0] == symbol && this.board[2][1] == symbol && this.board[2][2] == symbol){
-//                 return symbol;
-//             }
-//             else if (this.board[0][0] == symbol && this.board[1][0] == symbol && this.board[2][0] == symbol){
-//                 return symbol;
-//             }
-//             else if (this.board[0][1] == symbol && this.board[1][1] == symbol && this.board[2][1] == symbol){
-//                 return symbol;
-//             }
-//             else if (this.board[0][2] == symbol && this.board[1][2] == symbol && this.board[2][2] == symbol){
-//                 return symbol;
-//             }
-
-//             else if (this.board[0][0] == symbol && this.board[1][1] == symbol && this.board[2][2] == symbol){
-//                 return symbol;
-//             }
-//             else if (this.board[0][2] == symbol && this.board[1][1] == symbol && this.board[2][0] == symbol){
-//                 return symbol;
-//             }
-//         }
-//     }
-
-//     isFull(){
-//         for (let y = 0; y < this.board.length; y++){
-//             for (let x = 0; x < this.board[y].length; x++){
-//                 if (this.board[x][y] === ' '){
-//                     return false;
-//                 }
-//             }
-//         }
-//         return true;
-//     }
-
-//     isGameOver(){
-//         if (this.calcWinner() || this.isFull()){
-//             return true;
-//         }
-//         return false;
-//     }
-
-//     getValidXYCoord(player){
-//         while(true){
-//             document.getElementById('message').innerText = `${player.name}'s turn.  Please input x,y coordinates for ${player.token} symbol: `
-//             let user_input = prompt(`${player.name}'s turn.  Please input x,y coordinates for ${player.token} symbol: `);
-
-//             let x_input = user_input[0];
-//             let y_input = user_input[2];
-//             if(this.board[y_input][x_input] === ' '){
-//                 return [x_input, y_input]
-//             } else {
-//                 alert(`${x_input},${y_input} already taken!  Please enter new coordinates.`)
-//             }
-//         }
-//     }
-
-//     playTurn(player){
-//         let coord = this.getValidXYCoord(player);
-//         this.move(coord[0],coord[1],player.token);
-//     }
-// }
-// //repl loop
-// let run_flag = true
-// while (run_flag){
-
-//     let start = gameInit();
-//     let gameBoard = start[0];
-//     let player1 = start[1];
-//     let player2 = start[2];
-
-//     let counter = 1;
-
-//     while(!gameBoard.isGameOver()){
-//         if(counter % 2 == 1){
-//             gameBoard.playTurn(player1);
-//         } else if (counter % 2 == 0) {
-//             gameBoard.playTurn(player2);
-//         }
-//         counter++;
-//         gameBoard.printBoard();
-//     }
-
-//     if(gameBoard.calcWinner()){
-//         alert(`Player ${gameBoard.calcWinner()} wins!`);
-//     } else if (gameBoard.isFull()){
-//         alert(`The board is full...${player1.name} ties ${player2.name}`);
-//     }
-
-//     let user_input = prompt("Play again?  Please enter (y) or (n): ");
-//     if (user_input === 'n'){
-//         alert("Game over.");
-//         run_flag = false;
-//     }
-// }
