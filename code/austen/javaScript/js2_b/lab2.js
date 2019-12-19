8b8eb8ea4321f153f753fd8c8e2201edcd3c821b
austen <a_cote@comcast.net>
2019-12-19 11:41:11 -0800

    working pick 6 need css

--- code/austen/javaScript/js2_b/lab2.js
+++ code/austen/javaScript/js2_b/lab2.js
@@ -2,24 +2,41 @@ let winningTicket = document.getElementById("winningTicket");
 let guesses = document.getElementById("guesses");
 let play = document.getElementById("play");
 let totalBallance = document.getElementById("totalBallance");
+let resetGame = document.getElementById("reset");
+let ticketView = document.getElementById("ticketView").style.display = "none";
+let totals = document.getElementById("totals").style.display = "none";
 
-
-// creats a array of random numbers between 0-99
-var ticket = [];
-while(ticket.length < 6){
-    var r = Math.floor(Math.random() *100) +1;
-    if(ticket.indexOf(r) === -1) ticket.push(r);
-}
-if (ticket) {
-    winningTicket.innerText = ticket;
-}
-
-total_spent = -0;
+let ticket;
+let plays;
+let total_spent = -0;
 let ballance = 0;
+// creats a array of random numbers between 0-99
+function game(){
+    yourTicket();
+    tickets();
+    total_spent = -0;
+    ballance = 0;
+    totalSpent.innerText = total_spent;
+    totalBallance.innerText = ballance;
+    guesses.value = null;
+    document.getElementById("totals").style.display = "none"
+}
+function yourTicket(){
+    ticket = [];
+    while(ticket.length < 6){
+        var r = Math.floor(Math.random() *100) +1;
+        if(ticket.indexOf(r) === -1) ticket.push(r);
+    }
+    if (ticket) {
+        document.getElementById("ticketView").style.display = "block";
+        winningTicket.innerText = ticket;
+    }
 //while loop for 100000 tries to match the ticket with your_ticket
+}
+
 function tickets(){
     let num = 0;
-    let plays = guesses.value;
+    plays = guesses.value;
     while(num < plays){
     // incrimenting the ballance down 2 dollars totoal spent up by 2 dollars
         ballance -= 2;
@@ -31,14 +48,9 @@ function tickets(){
             var r = Math.floor(Math.random() *100) +1;
             if(your_ticket.indexOf(r) === -1) your_ticket.push(r);
         }
-        // console.log(your_ticket);
-        // var same = your_ticket.find(val => ticket.includes(val) );
-        // console.log(same)
     //checking to see if the numbers in both arrarys match up
         var same =(a1, a2) => your_ticket.reduce((a, c) => a + ticket.includes(c), 0);
-        // console.log(same(your_ticket,ticket));
     //giving them a if else if statment to inccrease the ballance and ticket winnings
-        // console.log(num);
         if (same(your_ticket,ticket) === 1) {
             ballance +=4;
         }else if (same(your_ticket,ticket) === 2) {
@@ -54,17 +66,12 @@ function tickets(){
         }
 
         num++;
-        
+        document.getElementById("totals").style.display = "block"
     }
-totalSpent.innerText = total_spent
+// this is putting the text on the page in the proper positions
+totalSpent.innerText = total_spent;
 totalBallance.innerText = ballance;
 }
-play.addEventListener('click',tickets)
-//writing it out on the page
-// document.write("<br> The total spent is: "+ '$' + total_spent);
-// document.write("<br> The ballance is: " +'$' + ballance);
-// document.write(same);
-//     // console.log(same)
-
-
-// }
+play.addEventListener('click', tickets);
+totalBallance.addEventListener('click', totalBallance);
+resetGame.addEventListener('click', game);
