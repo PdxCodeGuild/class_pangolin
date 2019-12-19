46936da6306adfc3886d785c8d1ac2fef5c104ec
clouddenizen <tr@clouddenizen.com>
2019-12-18 16:20:19 -0800

    Converter...converted?

--- code/taylor/JavaScript_Labs/Lab_01/Lab_A/javaScript.js
+++ code/taylor/JavaScript_Labs/Lab_01/Lab_A/javaScript.js
@@ -7,39 +7,32 @@ let conversions = {
   nchz:'.0254'
 }
 
-console.log(Object.values(conversions))
-console.log(Object.keys(conversions))
-
-
 let message1 = "Enter the length / distance as a number: > "
 let message2 = "Enter a starting uint as (ft, mi, km, m, yd, nchz): > "
 let message3 = "Enter a conversion uint as (ft, mi, km, m, yd, nchz): > "
 
 let user_starting_unit = prompt(message2)
+let user_number_input = prompt(message1);
+let user_unit_conversion_input = prompt(message3);
 
+function convertToMeters(conversions, user_number_input){
+Object.keys(conversions).forEach(function eachKey(key) { 
+    if (key === user_starting_unit){
+    return meters = (conversions[key] * user_number_input)
+    }
+  })
+};
 
-for (let key of Object.keys(conversions)) {
-  if (key == user_starting_unit)
-  console.log("here")
-}
+convertToMeters(conversions, user_number_input)
+console.log(meters)
 
-// let user_number_input = prompt(message1);
-// let user_unit_conversion_input = prompt(message3);
+function convertToSelectedUnit(conversions, user_unit_conversion_input){
+  Object.keys(conversions).forEach(function eachKey(key) { 
+      if (key === user_unit_conversion_input){
+      return conversion = ( meters / conversions[key])
+      }
+    })
+  };
 
-// alert("Return results here");
-
-// converts usre inputs to meters
-// function conver_to_meters(param) {  
-
-// }
-
-// converts from meters to desired unit
-// function conver_to_meters(param) {  }
-
-// for key, value in conversions.items():
-//      if key == user_starting_unit:
-//          to_meters = value * user_number_input
-
-// for key, value in conversions.items():
-// if key == user_unit_conversion_input:
-//           print(to_meters / value)
+convertToSelectedUnit(conversions, user_unit_conversion_input)
+console.log(conversion)
