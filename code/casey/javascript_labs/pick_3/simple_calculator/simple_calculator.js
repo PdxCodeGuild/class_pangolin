6c1e15c0c13d11ef84f0731140148710c507d4ac
Casey Burge <casey.patrick.burge@gmail.com>
2019-12-19 16:11:57 -0800

    Javascript todo lab & calculator

--- code/casey/javascript_labs/pick_3/simple_calculator/simple_calculator.js
+++ code/casey/javascript_labs/pick_3/simple_calculator/simple_calculator.js
@@ -2,24 +2,30 @@ let num1 = document.getElementById("num1");
 let num2 = document.getElementById("num2");
 let oper = document.getElementById("oper");
 let calc = document.getElementById("btn");
+let results = document.getElementById("results");
 
 function calculate() {
-    if (document.getElementById('oper').value === "+") {
-        console.log(num1 + num2);
-        return num1 + num2;
+    let x = parseFloat(num1.value);
+    let y = parseFloat(num2.value);
+    var answer
+    if (oper.value === "+") {
+        answer = x + y;
     }
-    if (document.getElementById('oper').value === "-") {
-        console.log(num1 - num2);
-        return num1 - num2;
+    else if (oper.value === "-") {
+        answer = x - y;
     }
-    if (document.getElementById('oper').value === "/") {
-        console.log(num1 / num2);
-        return num1 / num2;
+    else if (oper.value === "/") {
+        answer = x / y;
     }
-    if (document.getElementById('oper').value === "*") {
-        console.log(num1 * num2);
-        return num1 * num2;
+    else if (oper.value === "*") {
+        answer = x * y;
+    }
+    if (answer) {
+        results.innerText = answer;
     }
 };
 
-calc.addEventListener("click", calculate());
+// num1.addEventListener("input", calculate);
+// oper.addEventListener("input", calculate);
+// num2.addEventListener("input", calculate);
+calc.addEventListener("click", calculate);
