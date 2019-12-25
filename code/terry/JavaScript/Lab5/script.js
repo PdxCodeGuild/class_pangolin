d3dcd0b1ef6f9e4d97a075e95318c525e0daf6b5
tlfuller <tlfuller@rocketmail.com>
2019-12-24 17:57:24 -0800

    minor changes

--- code/terry/JavaScript/Lab5/script.js
+++ code/terry/JavaScript/Lab5/script.js
@@ -1,12 +1,11 @@
 let submit = document.getElementById('submit');
 
-let urlArray = ['https://www.google.com', 'https://www.hotmail.com', 'https://www.gmail.com'];
+let urlArray = ['https://www.google.com', 'https://www.outlook.com', 'https://www.gmail.com'];
 
 let rand = urlArray[(Math.random() * urlArray.length) | 0];
 
 submit.addEventListener('click', function() {
-    // console.log(rand);
-    // myFunction();
+
     time = 5, r = document.getElementById('result'), tmp = time;
 
     setInterval(function() {
@@ -16,7 +15,6 @@ submit.addEventListener('click', function() {
         if (s == 1) {
             location.replace(rand);
         } else {
-            console.log(s);
             r.textContent = 'Page redirect in ' + (s.length > 1 ? '' : '0') + s
             tmp != 0 || (tmp = time);
         }
