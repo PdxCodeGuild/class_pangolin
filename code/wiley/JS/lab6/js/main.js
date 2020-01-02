45c68484696466bc652253f9a0224c618eec7afb
Wiley R <wiley.rummel@gmail.com>
2020-01-02 08:02:40 -0800

    Need to remove old laps

--- code/wiley/JS/lab6/js/main.js
+++ code/wiley/JS/lab6/js/main.js
@@ -3,7 +3,7 @@ var toggleBtn = document.getElementById('toggle');
 var resetBtn = document.getElementById('reset');
 var lapBtn = document.getElementById("lap");
 var lapContainer = document.getElementById("laps");
-var lapTimes = document.getElementsByClassName('lapTimes');
+// var lapTimes = document.getElementsByClassName('lapTimes');
 
 var watch = new Stopwatch(timer);
 var lapCounter = 0;
@@ -20,7 +20,11 @@ toggleBtn.addEventListener('click', function(){
 resetBtn.addEventListener('click', function(){
     watch.reset();
     // var lapTimes = document.getElementsByClassName('lapTimes');
-    console.log(lapTimes)
+    var lapTimes = document.getElementById('laps').nodeType;
+    console.log(lapTimes);
+    lapTimes.forEach(element => {
+        element.remove()
+    });
     //need to figure out how to remove previous laps upon reset
 
 });
