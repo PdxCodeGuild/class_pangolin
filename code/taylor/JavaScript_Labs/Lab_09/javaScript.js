fa90f0bdc9b45b6a797d8d26cf40b7cb9924e486
clouddenizen <tr@clouddenizen.com>
2020-01-02 15:01:26 -0800

    lab_09 template complete

---
+++ code/taylor/JavaScript_Labs/Lab_09/javaScript.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+// #####################################################################
+// NEEDS EVENT LISTINER ON CLICK
+// FORM AND BUTTON INFO
+
+// <form class="form-inline d-flex">
+// <input type="text" class="form-control flex-fill mr-0 mr-sm-2 mb-3 mb-sm-0" id="inputSearch" placeholder="Search term here...">
+// <button type="submit" class="btn btn-primary mx-auto">Qtrieve</button>
+// </form>
+
+
+
+// #####################################################################
+// NEEDS TO BUILD THESE AS QOUTE OUTPUT
+
+//       <div id="allQuotes"class="row">
+
+
+//       <div class="col-md-4 mb-3 mb-md-0">
+//         <div class="card py-4 h-100">
+//           <div class="card-body text-center">
+
+//             <h4 class="text-uppercase m-0">Quote Author</h4>
+//             <hr class="my-4">
+//             <div class="small text-black-50">QUOTE - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate animi itaque vel quo saepe? Similique corrupti laudantium veniam magni? Asperiores maxime suscipit aliquam aut sit adipisci possimus voluptatum. Voluptatem, harum?</div>
+//           </div>
+//         </div>
+//       </div>
