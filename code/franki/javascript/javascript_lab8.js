0ec34415d72c55524cd255e9708b2d864e1f4512
franki <franki.wangen@gmail.com>
2020-01-01 23:03:17 -0800

    bouncing ball

--- code/franki/javascript/javascript_lab8.js
+++ code/franki/javascript/javascript_lab8.js
@@ -8,25 +8,28 @@ ctx.strokeRect(20, 20, 860, 560)
 
 let ball = {
     radius: 30,
-    px: Math.random()*835,
-    py: Math.random()*535,
+    px: Math.random()*830,
+    py: Math.random()*530,
     vx: (2*Math.random()-1)*10,
     vy: (2*Math.random()-1)*10
 };
 
 function main_loop() {
     ctx.clearRect(25, 25, 850, 550);
-    // update the ball's position
+    ball.vy += .5;
     ball.px += ball.vx;
     ball.py += ball.vy;
 
-    if (ball.px > 835 || ball.px < 35) {
+    if (ball.px > 840 || ball.px < 60) {
         ball.vx = -ball.vx;
     }
 
-    if (ball.py > 535 || ball.py < 35) {
+    if (ball.py > 540 || ball.py < 60) {
         ball.vy = -ball.vy;
     }
+    
+        // ball.vx = ball.vx*.99;
+        // ball.vy = ball.vy*.99;
   
     // draw the ball
     ctx.beginPath();
