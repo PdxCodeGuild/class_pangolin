75c5e0b570a2a760ff2f0c211f829889777b0197
tlfuller <tlfuller@rocketmail.com>
2020-01-02 16:40:39 -0800

    update

---
+++ code/terry/JavaScript/Lab9/script.js
@@ -0,0 +1,32 @@
+let button = document.getElementById('button');
+let result = document.getElementById('result');
+let userInput = document.getElementById('userInput');
+
+button.addEventListener('click', myQuote);
+
+function myQuote() {
+    let input = userInput.value;
+    axios({
+            url: 'https://favqs.com/api/quotes',
+            method: 'GET',
+            headers: {
+                Authorization: 'Token token="898ce244ffea7807a375ce02f5a27f99"'
+            },
+            params: {
+
+                filter: input,
+                // type: 'tags'
+            },
+        })
+        .then(function(response) {
+            console.log(response);
+            Array.from(response.data.quotes).forEach(function(quote) {
+                let pageInput = `${quote.body}<p><em>--${quote.author}</em></p>`;
+                result.innerHTML = pageInput;
+            });
+
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
+};
