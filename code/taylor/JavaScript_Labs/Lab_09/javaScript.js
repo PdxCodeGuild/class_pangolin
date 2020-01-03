ead72bdffe57211f32336f1f2df9313580dca357
clouddenizen <tr@clouddenizen.com>
2020-01-03 12:34:42 -0800

    js card build

--- code/taylor/JavaScript_Labs/Lab_09/javaScript.js
+++ code/taylor/JavaScript_Labs/Lab_09/javaScript.js
@@ -1,29 +1,39 @@
 // Bot nom nom 44462dcda4bba9533e2d37b1b089f7b5
 
 
+
 // axios.get('/user?ID=12345')
 //   .then(function (response) {
 //     // handle success
 //     console.log(response);
 //   })
-//   .catch(function (error) {
-//     // handle error
-//     console.log(error);
-//   })
+  // .catch(function (error) {
+  //   // handle error
+  //   console.log(error);
+  // })
 //   .finally(function () {
 //     // always executed
 //   });
 
-axios({
-  method: 'get',
-  url: 'https://favqs.com/api/qotd',
-  headers: {
-    Authorization: 'Token token="44462dcda4bba9533e2d37b1b089f7b5"'
-  }
-})
-  .then(function (response) {
-    console.log(response)
-  });
+// axios({
+//   method: 'get',
+//   url: 'https://favqs.com/api/qotd',
+//   headers: {
+//     Authorization: 'Token token="44462dcda4bba9533e2d37b1b089f7b5"'
+//   }
+// })
+//   .then(function (response) {
+//     console.log(response);
+//   })
+//   .catch(function (error) {
+//     // handle error
+//     console.log(error);
+//   });
+
+
+
+
+
 
 
 
@@ -36,21 +46,41 @@ axios({
 // <button type="submit" class="btn btn-primary mx-auto">Qtrieve</button>
 // </form>
 
+// function testPrint(){
+//   console.log("I've been pressed")
+// }
 
+let searchSubmit = document.getElementById('searchSubmit');
 
-// #####################################################################
-// NEEDS TO BUILD THESE AS QOUTE OUTPUT
+searchSubmit.onclick = function() {
 
-//       <div id="allQuotes"class="row">
+// Retreives allQoutes element as target for appends
+  let allQuotes = document.getElementById('allQuotes')
 
+// Builds the div / card for all qoutes
+  let div_1 = document.createElement("div");
+  div_1.className = "col-md-4 mb-3 mb-md-3";
+  allQuotes.appendChild(div_1)
+  let div_2 = document.createElement('div');
+  div_2.className = "card py-4 h-100";
+  div_1.appendChild(div_2)
+  let div_3 = document.createElement('div');
+  div_3.className = "card-body text-center";
+  div_2.appendChild(div_3)
 
-//       <div class="col-md-4 mb-3 mb-md-0">
-//         <div class="card py-4 h-100">
-//           <div class="card-body text-center">
+// Builds the html recepticles for qoute author and qote
+  let h4 = document.createElement('h4');
+  h4.className = "text-uppercase m-0 text-wrap";
+  div_3.appendChild(h4)
+  let div_4 = document.createElement('div');
+  div_4.className = "small text-black-50";
+  div_3.appendChild(div_4)
 
-//             <h4 class="text-uppercase m-0">Quote Author</h4>
-//             <hr class="my-4">
-//             <div class="small text-black-50">QUOTE - Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate animi itaque vel quo saepe? Similique corrupti laudantium veniam magni? Asperiores maxime suscipit aliquam aut sit adipisci possimus voluptatum. Voluptatem, harum?</div>
-//           </div>
-//         </div>
-//       </div>
+// Adds content to the html build
+  h4.innerHTML = "ZZZZZZZZZZZZZZZZZZZZZZZZZZZ";
+  div_4.innerHTML = "TEST TESsd asdf sadfsdafsadfsadf sadfsdaf sadfsda fasd fsadf sad fsadfsadfT TEST";
+  h4.appendChild(h4)
+  div_4.appendChild(div_4)
+  console.log("SOMETHING HAPPENED >>>>")
+};
+
