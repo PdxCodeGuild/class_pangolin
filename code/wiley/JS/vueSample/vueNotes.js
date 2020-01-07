9817e3436bbf228357638d95a900ad3c59808b20
WileyRummel <wiley.rummel@gmail.com>
2020-01-07 12:50:09 -0800

    completed to do list, lab 10

--- code/wiley/JS/vueSample/vueNotes.js
+++ code/wiley/JS/vueSample/vueNotes.js
@@ -9,3 +9,23 @@ View model has a "shadow DOM"
 READ THE VIEW DOCS THEY ARE GOOD.
 Vue uses components, as modular blocks of codes to apply when needed.  
 */
+
+//COMPONENTS
+//start by defining a new component
+Vue.component('button-counter', {
+    data: function(){ //the data in a compenent is a function
+        return {
+            count:0
+        }
+    },
+    template: '<button v-on:click="count++"> You clicked me {{ count }} times. </button>' //the template is the html blue-print
+});
+//you can then call this into your HTML with a call like this:
+<div id="components-demo">
+        <button-counter></button-counter>
+</div>
+//then attach is to a new component in your script like this:
+new Vue({ el: '#components-demo' });
+
+//the element <slot> is used to add a value into a Vue element.  
+// if you use a custom Vue element, you can then add <sloy></slot> for where you want it to be. 
