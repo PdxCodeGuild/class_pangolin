0d8d1f54f3ed090e58e436e1e79a3b9bf62e5106
WileyRummel <wiley.rummel@gmail.com>
2020-01-08 13:47:17 -0800

    Revert "calculators suck"
    This reverts commit 7ef3553a94c89086a129ab2e59db7692a2e09aa0.

--- code/wiley/JS/lab12/js/main.js
+++ code/wiley/JS/lab12/js/main.js
@@ -31,88 +31,36 @@ let vm = new Vue({
     el: '#Calculator',
     data:function() {
     return {
-        number: '',
-        current: '',
+        number: 0,
         operator: "",
         total: 0,
-        operation: ""}
+        operationArray: []}
     },
     methods: {
         numClick: function(num){
-            //check for multiple . not currently in operation
-            // if (this.number[this.number.length-1] === "."){
-            //     this.number.slice(-1)
-            //     console.log("testing splice  " + this.number)
-            // }
             this.number += num
-            this.total = this.number
-            console.log("Num Click Console Test = ", this.number +" <-number total -> "+  this.total)
         },
         operClick: function(oper){
             this.operator = oper;
-            console.log(this.current)
-            this.total = this.current;
-            this.current = this.number;
-            this.number = '';
-            console.log("Num Click Console Test = ", this.current +" <-current total -> "+  this.total);
-            console.log(this.operator)
-           
-
-            if (this.operator === "AC"){ //works
-                this.operation = "",
-                this.number = "",
-                this.total = ""
-            }
-            
-            else if (this.operator === "+/-"){ //works
-                this.number = parseFloat(this.number)
-                this.number *= -1
-                this.total = this.number
+            if (this.operator === "AC"){
+                this.operationArray = [],
+                this.number = 0
+                console.log(this.operationArray)
             }
             else if (this.operator === "="){
-                console.log("this is the operation" + this.operation)
-                if (this.operation === "+"){
-                    this.number2 = this.number
-                    this.total = parseFloat(this.current) + parseFloat(this.total)
-                    console.log(isNaN(this.total))
-                    this.current = this.total
-                }
-                else if (this.operation === "-"){
-                    console.log("total is " + this.total, "number is " + this. number)
-                    this.total = parseFloat(this.total) - parseFloat(this.current)
-                    this.current = this.total
-
-                }
-                else if (this.operation === "*"){
-                    console.log("total is " + this.total, "number is " + this. number)
-                    this.total = parseFloat(this.total) * parseFloat(this.current)
-                    this.current = this.total
-
-                }
-                else if (this.operation === "/"){
-                    console.log("total is " + this.total, "number is " + this. number)
-                    this.total = parseFloat(this.total) / parseFloat(this.current)
-                    this.current = this.total
-
-                }
-                else {
-                    this.total = "ERROR"
-                }
-                
-                // this.number = "";
+                this.operationArray.push(this.number)
+                //parse through operationArray and complete operations.
+                this.total = eval(this.operationArray)
+                console.log(this.total)
+                // this.operationArray = this.operationArray.map(x => parseFloat(x))
             }
             else {
-                this.operation = oper
-            }
-        },
-        percentClick: function(num){ //works
-            console.log(this.number)
-            this.number = parseFloat(this.number);
-            this.number /= 100;
-            this.total = this.number;
-            this.number = "";
-        },
-        
+                this.operationArray.push(this.number);
+                this.operationArray.push(this.operator);
+                // this.operationArray.push(this.operator);
+                console.log(this.operationArray)
+                this.number = "";}
+        }
 
 
     },
@@ -123,31 +71,3 @@ let vm = new Vue({
         console.log("ITS ALIVE!")
     }
 });
-
-
-
-//trash
-// this.operationArray.push(this.number);
-                // this.operationArray.reduce(function(a,b){
-                //     if (this.operationArray.length >= 3){
-                //         console.log("this is running?")
-                //         return this.operationArray[a[1]](parseFloat(a[0]),(parseFloat(a[2])));
-                //     } else {
-                //         console.log(a)
-                //         return a.push(b)}
-                // })
-                //parse through operationArray and complete operations.
-                // this.total = eval(this.operationArray)
-// addClick: function(num){
-    
-//     this.number = (a,b) => a+b;
-// },
-// subractClick: function(a,b) {
-//     this.number = (a-b)
-// },
-// divideClick:function(a,b) {
-//     this.number = (a/b)
-// },
-// multiplyClick: function(a,b) {
-//     this.number = (a*b)
-// }
