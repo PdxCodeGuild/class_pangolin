deab68002ee6dfa63ddb5fda9a3d191bf5cb804d
clouddenizen <tr@clouddenizen.com>
2020-01-10 13:44:43 -0800

    update

--- code/taylor/JavaScript_Labs/Lab_13/javaScript.js
+++ code/taylor/JavaScript_Labs/Lab_13/javaScript.js
@@ -116,3 +116,31 @@ function buildSearchedQuoteCards(author, quote) {
   h4.innerHTML = author;
   div_4.innerHTML = quote;
 };
+
+
+
+let vm = new Vue({
+  el: "#app",
+  data: {
+    routeInput: "",
+    results: []
+  },
+  methods: {
+    getBuses: function() {
+      axios({
+        method: "get",
+        url: "https://developer.trimet.org/ws/v2/vehicles",
+        params: {
+          appID: apiKey,
+          routes: this.routeInput
+        }
+      }).then(res => this.results = res.data.resultSet.vehicle);
+    }
+  }
+});
+
+/*
+secrets.js
+
+let apiKey = "D065A3A5DAE4622752786CEB9";
+*/
