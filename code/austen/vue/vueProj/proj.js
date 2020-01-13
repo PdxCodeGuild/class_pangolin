8463e06a7dc47264e66a08a74d98924ea661eb5e
austen <a_cote@comcast.net>
2020-01-12 22:40:45 -0800

    added ticketmaster api still working show

--- code/austen/vue/vueProj/proj.js
+++ code/austen/vue/vueProj/proj.js
@@ -45,18 +45,48 @@ let vm = new Vue ({
     el:'#app',
     data: function() {
         return {
-            info: []
+            info: [],
+            event: ''
         }
     },
-    mounted () {
-        axios({
-            method: 'get',
-            url:'https://app.ticketmaster.com/discovery/v2/events?apikey=ILjk0YWDkMs9CyCjpRCsSMqCs5P2vjGd&locale=*',
-            async: true,
-            // headers:{
-            //     Authorization: 'Bearer 5BRHG6QOBATA4BU5K6'
-            // }
-        }).then(response=>(this.info=response),
-        console.log(this.info))
+    methods:{
+        getData: function() {
+            let token= "SMGqCoSLZO-CnmSN28MitIBPHTxQhaxcQmd7nFWURV6NKSu_xJwkPylSSfPJfcoafykRCg51GCNUA1m_fEeJej5uT0wfQZJMaZKRd1R2nY-jsRlNVt0MEcQyMGIaXnYx"
+            axios({
+                method: 'get',
+                url:'https://api.yelp.com/v3/events/featured',
+                headers:{
+                    'Authorization': 'Bearer'+ SMGqCoSLZO-CnmSN28MitIBPHTxQhaxcQmd7nFWURV6NKSu_xJwkPylSSfPJfcoafykRCg51GCNUA1m_fEeJej5uT0wfQZJMaZKRd1R2nY-jsRlNVt0MEcQyMGIaXnYx 
+                }, 
+            }).then(response=>(console.log(response),this.info=response),
+            console.log(this.info))
+        },
+        getStuff: function() {
+            axios({
+                method: "get",
+                url: "https://www.boredapi.com/api/activity/",
+            })
+            .then(response => {(this.info = response.data);
+            parseFloat(this.info.price),
+            console.log(this.info);
+            console.log(this.info.price);
+
+        })
+        },
+        getTickets: function () {
+            axios({
+                method: "get",
+                url: "https://app.ticketmaster.com/discovery/v2/events.json?classificationName=&dmaId=362&apikey=ILjk0YWDkMs9CyCjpRCsSMqCs5P2vjGd",
+                // params: {
+                //     filter: this.event.events
+                // },
+                async:true
+            })
+            .then(response => {this.event = response.data._embedded.events})
+            this.$emit('newResults', this.event)
+        },
+        mounted: function() {
+            console.log("It loaded!");
+         }
     }
 });
