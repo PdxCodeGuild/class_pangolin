f6ec6dbaeb37bd4d5f8ecc18ecd26bfcaa3521f2
Shawn Stolsig <shawnstolsig@gmail.com>
2020-01-13 19:42:39 -0800

    not pulling student list

---
+++ code/shawn/django/lab06-drf_vue/api/serializers.py
@@ -0,0 +1,7 @@
+from students.models import Student
+from rest_framework import serializers
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = ('first_name', 'last_name', 'cohort', 'favorite_topic', 'favorite_teacher', 'capstone')
