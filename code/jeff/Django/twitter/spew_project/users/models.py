ed8b9ddea55d69a52c9b331a701c69a1fc750f45
Jeff <jsportland@gmail.com>
2020-01-17 10:01:38 -0800

    django twitter clone

---
+++ code/jeff/Django/twitter/spew_project/users/models.py
@@ -0,0 +1,11 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    image = models.ImageField(
+        default='profile_pics/default.jpg', upload_to='profile_pics')
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
