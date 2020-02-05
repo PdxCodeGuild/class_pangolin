9f93691fc55c4f88ea8a28eaed0c06ee213edfee
Jeff <jsportland@gmail.com>
2020-02-05 09:54:57 -0800

    student

---
+++ code/jeff/student/apis/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+from students.models import Student
+
+
+class StudentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Student
+        fields = (
+            'id',
+            'first_name',
+            'last_name',
+            'cohort',
+            'favorite_topic',
+            'favorite_teacher',
+            'capstone'
+        )
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = (
+            'username',
+            'email'
+        )
