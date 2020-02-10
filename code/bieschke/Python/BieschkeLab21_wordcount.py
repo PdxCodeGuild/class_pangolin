dfd39f7685a2b6ea0674ebc0c798432d2831dab0
Brandon Bieschke <mistahbieschke@gmail.com>
2020-02-10 10:58:24 -0800

    Lab 21 completed from the sunniest place you'd ever love

---
+++ code/bieschke/Python/BieschkeLab21_wordcount.py
@@ -0,0 +1,30 @@
+import string
+
+diction = {}
+
+with open('gettysburg.txt', 'r') as file:
+    lines = file.read().lower()
+
+stopwords = ['i', 'me', 'my', 'myself', 'we', 'our', 'ours', 'ourselves', 'you', "you're", "you've", "you'll", "you'd", 'your', 'yours', 'yourself', 'yourselves', 'he', 'him', 'his', 'himself', 'she', "she's", 'her', 'hers', 'herself', 'it', "it's", 'its', 'itself', 'they', 'them', 'their', 'theirs', 'themselves', 'what', 'which', 'who', 'whom', 'this', 'that', "that'll", 'these', 'those', 'am', 'is', 'are', 'was', 'were', 'be', 'been', 'being', 'have', 'has', 'had', 'having', 'do', 'does', 'did', 'doing', 'a', 'an', 'the', 'and', 'but', 'if', 'or', 'because', 'as', 'until', 'while', 'of', 'at', 'by', 'for', 'with', 'about', 'against', 'between', 'into', 'through', 'during', 'before', 'after', 'above', 'below', 'to', 'from', 'up', 'down', 'in', 'out', 'on', 'off', 'over', 'under', 'again', 'further', 'then', 'once', 'here', 'there', 'when', 'where', 'why', 'how', 'all', 'any', 'both', 'each', 'few', 'more', 'most', 'other', 'some', 'such', 'no', 'nor', 'not', 'only', 'own', 'same', 'so', 'than', 'too', 'very', 's', 't', 'can', 'will', 'just', 'don', "don't", 'should', "should've", 'now', 'd', 'll', 'm', 'o', 're', 've', 'y', 'ain', 'aren', "aren't", 'couldn', "couldn't", 'didn', "didn't", 'doesn', "doesn't", 'hadn', "hadn't", 'hasn', "hasn't", 'haven', "haven't", 'isn', "isn't", 'ma', 'mightn', "mightn't", 'mustn', "mustn't", 'needn', "needn't", 'shan', "shan't", 'shouldn', "shouldn't", 'wasn', "wasn't", 'weren', "weren't", 'won', "won't", 'wouldn', "wouldn't"]
+
+translator = str.maketrans('', '', string.punctuation)
+string_without_punct = lines.translate(translator)
+list_of_words = string_without_punct.split()
+
+for w in list_of_words:
+    if w in stopwords:
+        pass
+    elif w in diction:
+        diction[w] +=1
+    else:
+        diction[w] = 1
+
+print("Here's a list of words used in the text")
+print(f"{diction}\n")
+
+# diction is a dictionary where the key is the word and the value is the count
+print("These are the top 10 most used words in the text!")
+words = list(diction.items()) # .items() returns a list of tuples
+words.sort(key=lambda tup: tup[1], reverse=True)  # sort largest to smallest, based on count
+for i in range(min(10, len(words))):  # print the top 10 words, or all of them, whichever is smaller
+    print(words[i])
